e2cf150ccc885c1f349f3b15fc7fac1ac833aa4c
Andreas Schempp <andreas@schempp.ch>
2009-11-13 12:49:59 +0000

    runonce.php migrates Isotope to new features.

---
+++ system/runonce.php
@@ -0,0 +1,70 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight webCMS
+ * Copyright (C) 2005 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at http://www.gnu.org/licenses/.
+ *
+ * PHP version 5
+ * @copyright  Winans Creative 2009
+ * @author     Andreas Schempp <andreas@schempp.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+class Isotope extends Frontend
+{
+
+	/**
+	 * Initialize the object
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->import('Database');
+	}
+
+
+	/**
+	 * Run the controller
+	 */
+	public function run()
+	{
+		$this->updateAttributes();
+	}
+	
+	
+	/**
+	 * Checkboxes should be '' not '0'
+	 */
+	private function updateAttributes()
+	{
+		$arrFields = $this->Database->listFields('tl_product_attributes');
+		foreach( $arrFields as $field )
+		{
+			$this->Database->execute("UPDATE tl_product_attributes SET " . $field['name'] . "='' WHERE " . $field['name'] . "='0'");
+		}
+	}
+}
+
+
+/**
+ * Instantiate controller
+ */
+$objIsotope = new Isotope();
+$objIsotope->run();
+
