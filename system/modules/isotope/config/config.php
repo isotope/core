31d0bd6478382a1119ed6cb72fec01dd49a02e2a
Andreas Schempp <andreas@schempp.ch>
2009-11-22 13:56:17 +0000

    Dropped use_price_override.

--- system/modules/isotope/config/config.php
+++ system/modules/isotope/config/config.php
@@ -307,31 +307,6 @@ $GLOBALS['ISO_ATTR'] = array
 		'delete_locked'				=> 1
 	),
 	
-	// Use Product Price Override	
-	array
-	( 
-		
-		'pid'						=> 0,
-		'tstamp'					=> time(),
-		'type'						=> 'checkbox',
-		'field_name'				=> 'use_price_override',
-		'legend'					=> 'pricing_legend',
-		'name'						=> 'Use Product Price Override',
-		'description'				=> '',
-		'attr_use_mode'				=> 'fixed',
-		'is_customer_defined'		=> 0,
-		'is_visible_on_front'		=> 0,
-		'is_required'				=> 0,
-		'is_filterable'				=> 0,
-		'is_searchable'				=> 0,
-		'is_used_for_price_rules'	=> 0,
-		'is_multiple_select'		=> 0,
-		'use_rich_text_editor'		=> 0,
-		'is_user_defined'			=> 0,
-		'is_listing_field'			=> 0,
-		'delete_locked'				=> 1
-	),	
-
 	// Shipping Class		
 	array
 	( 
