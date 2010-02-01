77f992a9ae81d454254bbeedcee8adf191b75f7d
Fred Bliss <fred.bliss@intelligentspark.com>
2010-02-01 20:48:01 +0000

    switched date-specific sorting language

--- system/modules/isotope/languages/en/default.php
+++ system/modules/isotope/languages/en/default.php
@@ -202,8 +202,8 @@ $GLOBALS['TL_LANG']['MSC']['low_to_high'] = 'lo to hi';
 $GLOBALS['TL_LANG']['MSC']['high_to_low'] = 'hi to lo';
 $GLOBALS['TL_LANG']['MSC']['a_to_z'] = 'A to Z';
 $GLOBALS['TL_LANG']['MSC']['z_to_a'] = 'Z to A';
-$GLOBALS['TL_LANG']['MSC']['old_to_new'] = 'old to new';
-$GLOBALS['TL_LANG']['MSC']['new_to_old'] = 'new to old';
+$GLOBALS['TL_LANG']['MSC']['old_to_new'] = 'later to earlier';
+$GLOBALS['TL_LANG']['MSC']['new_to_old'] = 'earlier to later';
 
 
 
