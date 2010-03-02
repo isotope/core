63498fa7fb55ea4442f40bda446a8ef4eb2cf0e6
Andreas Schempp <andreas@schempp.ch>
2010-03-02 21:41:40 +0000

    More multilingual stuff

--- system/modules/isotope/languages/en/default.php
+++ system/modules/isotope/languages/en/default.php
@@ -83,6 +83,7 @@ $GLOBALS['TL_LANG']['MSC']['deleteLanguage']		= 'Delete';
 $GLOBALS['TL_LANG']['MSC']['defaultLanguage']		= 'Generic';
 $GLOBALS['TL_LANG']['MSC']['editingLanguage']		= 'ATTENTION: You are editing language-specific data!';
 $GLOBALS['TL_LANG']['MSC']['deleteLanguageConfirm']	= 'Are you sure you want to delete this language? There is no undo!';
+$GLOBALS['TL_LANG']['MSC']['undefinedLanguage']		= 'undefined';
 
 $GLOBALS['TL_LANG']['MSC']['ajaxLoadingMessage'] = 'Loading...';
 $GLOBALS['TL_LANG']['MSC']['scalingImageWidth'] = '1200';
