4a12db811014ae004d156ae6ec5165efe76d736b
Andreas Schempp <andreas@schempp.ch>
2010-03-22 09:56:02 +0000

    Added checkout label for payment surcharge

--- system/modules/isotope/languages/de/default.php
+++ system/modules/isotope/languages/de/default.php
@@ -69,6 +69,7 @@ $GLOBALS['TL_LANG']['MSC']['confirmOrder']	= 'Bestellen';
 $GLOBALS['TL_LANG']['MSC']['subTotalLabel'] = 'Zwischensumme';
 $GLOBALS['TL_LANG']['MSC']['grandTotalLabel'] = 'Gesamttotal';
 $GLOBALS['TL_LANG']['MSC']['shippingLabel'] = 'Versandkosten';
+$GLOBALS['TL_LANG']['MSC']['paymentLabel'] = 'Zahlungskosten';
 $GLOBALS['TL_LANG']['MSC']['taxLabel'] = 'Enthaltene MwSt: ';
 
 $GLOBALS['TL_LANG']['MSC']['noPaymentModules'] = 'Zur Zeit sind keine Zahlungsoptionen verfügbar.';
