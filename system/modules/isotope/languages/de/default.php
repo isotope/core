565718b9b9a00f184433185249c152ad0aa5ca9e
Andreas Schempp <andreas@schempp.ch>
2010-03-22 13:18:01 +0000

    Dimension product is now working. Also added german price range label.

--- system/modules/isotope/languages/de/default.php
+++ system/modules/isotope/languages/de/default.php
@@ -43,6 +43,7 @@ $GLOBALS['TL_LANG']['ERR']['orderNotFound']				= 'Die gewünschte Bestellung wur
 /**
  * Miscellaneous
  */
+$GLOBALS['TL_LANG']['MSC']['priceRangeLabel'] = 'ab';
 $GLOBALS['TL_LANG']['MSC']['removeProductLinkText'] = 'entfernen';
 $GLOBALS['TL_LANG']['MSC']['checkoutBT']				= 'Proceed to Checkout';
 $GLOBALS['TL_LANG']['MSC']['continueShoppingBT']		= 'Continue shopping';
