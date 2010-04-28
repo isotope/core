5918c8a80e49334f3bc2e8a5a35c601622fe4b0f
Andreas Schempp <andreas@schempp.ch>
2010-04-28 13:39:08 +0000

    Added tools menu to tl_product_data global buttons. This allows us to have many more buttons!

--- system/modules/isotope/languages/en/tl_product_data.php
+++ system/modules/isotope/languages/en/tl_product_data.php
@@ -83,21 +83,24 @@ $GLOBALS['TL_LANG']['tl_product_data']['existing_option_set'] = 'Select an exist
 $GLOBALS['TL_LANG']['tl_product_data']['new_option_set'] = 'Create a new product option set';
 
 $GLOBALS['TL_LANG']['tl_product_data']['variantValuesLabel'] = 'Variant';
+
+
 /**
  * Buttons
  */
 $GLOBALS['TL_LANG']['tl_product_data']['new']			= array('New product', 'Create new product.');
 $GLOBALS['TL_LANG']['tl_product_data']['edit']			= array('Edit product', 'Edit product ID %s');
+$GLOBALS['TL_LANG']['tl_product_data']['quick_edit']	= array('Quick-edit variants', 'Quick-edit variants for product ID %s');
 $GLOBALS['TL_LANG']['tl_product_data']['downloads']		= array('Downloads', 'Edit downloads for product ID %s');
 $GLOBALS['TL_LANG']['tl_product_data']['copy']			= array('Copy product', 'Copy product ID %s');
 $GLOBALS['TL_LANG']['tl_product_data']['cut']			= array('Move product', 'Move product ID %s');
 $GLOBALS['TL_LANG']['tl_product_data']['delete']		= array('Delete product', 'Delete product ID %s');
 $GLOBALS['TL_LANG']['tl_product_data']['show']			= array('Product details', 'Show details of product ID %s');
-$GLOBALS['TL_LANG']['tl_product_data']['related']		= array('Related products', 'Manage related products for product ID %s');
 $GLOBALS['TL_LANG']['tl_product_data']['generate']		= array('Generate variants', 'Generate variants for product ID %s');
+$GLOBALS['TL_LANG']['tl_product_data']['related']		= array('Related products', 'Manage related products for product ID %s');
+$GLOBALS['TL_LANG']['tl_product_data']['tools']			= array('Tools', 'More options for product management.');
 $GLOBALS['TL_LANG']['tl_product_data']['import']		= array('Import assets', 'Import images and other media from a folder');
 $GLOBALS['TL_LANG']['tl_product_data']['link']			= array('Link Products to Categories', 'Link products to categories');
-$GLOBALS['TL_LANG']['tl_product_data']['quick_edit']	= array('Quick-edit variants', 'Quick-edit variants for product ID %s');
 
 
 /** 
