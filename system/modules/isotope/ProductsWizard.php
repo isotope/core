cd5c65cf656bc56aa31c3de1c85161024857a469
Fred Bliss <fred.bliss@intelligentspark.com>
2010-06-29 12:58:17 +0000

    fixed a concatenation bug in the JS-disabled SQL statement

--- system/modules/isotope/ProductsWizard.php
+++ system/modules/isotope/ProductsWizard.php
@@ -125,7 +125,7 @@ class ProductsWizard extends Widget
 		// User has javascript disabled an clicked on link
 		if ($this->Input->get('noajax'))
 		{
-			$strProducts = $this->listProducts($this->Database->execute("SELECT * FROM tl_iso_products WHERE pid=0 ORDER BY id=" . implode('DESC, id=', $arrIds) . " DESC, name"));
+			$strProducts = $this->listProducts($this->Database->execute("SELECT * FROM tl_iso_products WHERE pid=0 ORDER BY id=" . implode(' DESC, id=', $arrIds) . " DESC, name"));
 		}
 		else
 		{
