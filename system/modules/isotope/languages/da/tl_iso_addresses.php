fc48604cd78bed5e19c986dc9675f6c59b59e6f2
Andreas Schempp <andreas@schempp.ch>
2010-07-06 07:25:08 +0000

    Synced danish language from betatest.winanscreative.com

---
+++ system/modules/isotope/languages/da/tl_iso_addresses.php
@@ -0,0 +1,71 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Winans Creative 2009, Intelligent Spark 2010, iserv.ch GmbH 2010
+ * @author     Stefan Johannsen <stefan@reklamehuset.dk>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+$GLOBALS['TL_LANG']['tl_iso_addresses']['firstname'][0] = 'Fornavn';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['firstname'][1] = 'Indtast venligst fornavn';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['lastname'][0] = 'Efternavn';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['lastname'][1] = 'Indtast venligst efternavn';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['company'][0] = 'Firma';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['company'][1] = 'Indtast evt. et firmanavn';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_1'][0] = 'Adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_1'][1] = 'Indtast venligst gadenavn og husnummer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_2'][0] = 'Adresse 2';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_2'][1] = 'Indtast evt. yderligere adresseinformationer hvis nødvendigt';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_3'][0] = 'Adresse 3';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_3'][1] = 'Indtast evt. yderligere adresseinformationer hvis nødvendigt';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['postal'][0] = 'Postnummer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['postal'][1] = 'Indtast venligst postnummer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['city'][0] = 'By';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['city'][1] = 'Indtast venligst by';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['country'][0] = 'Land';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['country'][1] = 'Vælg venligst et land';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['phone'][0] = 'Telefonnummer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['phone'][1] = 'Indtast venligst et telefonnummer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['email'][0] = 'E-mail adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['email'][1] = 'Indtast venligst en gyldig e-mail adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['isDefaultBilling'][0] = 'Standard betalingsadresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['isDefaultBilling'][1] = 'Er dette din standard betalingsadresse?';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['isDefaultShipping'][0] = 'Standard leveringsadresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['isDefaultShipping'][1] = 'Er dette din standard leveringsadresse?';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['personal_legend'] = 'Personlig information';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['address_legend'] = 'Adresseoplysninger';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['contact_legend'] = 'Kontaktinformationer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['default_legend'] = 'Standard adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['personalData'] = 'Personlig information';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['addressDetails'] = 'Adresseoplysninger';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['contactDetails'] = 'Kontaktinformationer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['loginDetails'] = 'Standard adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['new'][0] = 'Ny adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['new'][1] = 'Opret en ny adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['show'][0] = 'Adresseinformationer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['show'][1] = 'Vis detaljer for adressen %s';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['edit'][0] = 'Rediger adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['edit'][1] = 'Rediger adressen %s';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['copy'][0] = 'Kopier adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['copy'][1] = 'Kopier adressen %s';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['delete'][0] = 'Slet adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['delete'][1] = 'Slet adressen %s';
+
