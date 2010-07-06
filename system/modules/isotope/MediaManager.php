cec1cc2c198068937c7d00408999241c31546fb6
Andreas Schempp <andreas@schempp.ch>
2010-07-06 13:47:25 +0000

    Do not add an MD5 unique key to filename if not necessary.

--- system/modules/isotope/MediaManager.php
+++ system/modules/isotope/MediaManager.php
@@ -162,13 +162,19 @@ class MediaManager extends Widget implements uploadable
 			$this->import('Database');
 			
 			$pathinfo = pathinfo($file['name']);
+			$uploadFolder = 'isotope/' . strtolower(substr($pathinfo['filename'], 0, 1));
 		
 			// Make sure directory exists
-			$this->Files->mkdir('isotope/' . strtolower(substr($pathinfo['filename'], 0, 1)) . '/');
+			$this->Files->mkdir($uploadFolder);
 			
-			$strCacheName = $pathinfo['filename'] . '-' . substr(md5_file($file['tmp_name']), 0, 8) . '.' . $pathinfo['extension'];
+			$strCacheName = $pathinfo['filename'] . '.' . $pathinfo['extension'];
 			
-			$this->Files->move_uploaded_file($file['tmp_name'], 'isotope/' . strtolower(substr($pathinfo['filename'], 0, 1)) . '/' . $strCacheName);
+			if (is_file(TL_ROOT . '/' . $uploadFolder . '/' . $strCacheName) && md5_file($file['tmp_name']) != md5_file(TL_ROOT . '/' . $uploadFolder . '/' . $strCacheName))
+			{
+				$strCacheName = $pathinfo['filename'] . '-' . substr(md5_file($file['tmp_name']), 0, 8) . '.' . $pathinfo['extension'];
+			}
+			
+			$this->Files->move_uploaded_file($file['tmp_name'], $uploadFolder . '/' . $strCacheName);
 			
 			if (!is_array($this->varValue))
 			{
