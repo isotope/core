8b3ef747c8a0a5205fef4ec68dcb40d8d6310081
Andreas Schempp <andreas@schempp.ch>
2010-08-05 20:23:45 +0000

    cleaning

--- system/modules/isotope/IsotopeFrontend.php
+++ system/modules/isotope/IsotopeFrontend.php
@@ -55,9 +55,8 @@ class IsotopeFrontend extends Frontend
 	{
 		$intQuantity = ($objModule->iso_use_quantity && intval($this->Input->post('quantity_requested')) > 0) ? intval($this->Input->post('quantity_requested')) : 1;
 		
-		// HOOK for adding additional functionality to the addToCart operation
 		if ($this->Isotope->Cart->addProduct($objProduct, $intQuantity))
-		{			
+		{
 			$this->jumpToOrReload($objModule->iso_addProductJumpTo);
 		}
 	}
