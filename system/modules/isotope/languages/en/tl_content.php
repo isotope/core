f68283b3e50083fb7131b6c29107d85099f0cdf5
Andreas Schempp <andreas@schempp.ch>
2010-09-06 18:32:49 +0000

    Added new ContentIsotope abstract class for building content elements. Updated isotope_quickproducts to use this class.

--- system/modules/isotope/languages/en/tl_content.php
+++ system/modules/isotope/languages/en/tl_content.php
@@ -29,6 +29,8 @@
 /**
  * Fields
  */
+$GLOBALS['TL_LANG']['tl_content']['iso_reader_jumpTo']	= array('Product reader jump to page', 'This setting defines to which page a user will be redirected when clicking a product for more info.');
+$GLOBALS['TL_LANG']['tl_content']['iso_list_layout']	= array('Product list template', 'Please choose a list layout. You can add custom list layouts to folder <em>templates</em>. List template files start with <em>iso_list_</em> and require file extension <em>.tpl</em>.');
 $GLOBALS['TL_LANG']['tl_content']['iso_attribute_set'] = array('Product Attribute Set','Select a product attribute set to choose a listing filter for this page.');
 $GLOBALS['TL_LANG']['tl_content']['iso_filters'] = array('Listing Filter','Select a product listing filter');
 
