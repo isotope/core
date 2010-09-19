782f87823fb42004c3bcad14dd3635515b5133ee
Andreas Schempp <andreas@schempp.ch>
2010-09-19 10:54:58 +0000

    Fixed a bug when loading languages

--- system/modules/isotope/IsotopeProduct.php
+++ system/modules/isotope/IsotopeProduct.php
@@ -1081,7 +1081,7 @@ class IsotopeProduct extends Controller
 		
 		if ($objLanguage->numRows)
 		{
-			$arrAttributes = $this->arrData['pid'] > 0 ? $this->arrAttributes : $this->arrVariantAttributes;
+			$arrAttributes = $this->arrData['pid'] > 0 ? $this->arrVariantAttributes : $this->arrAttributes;
 			
 			foreach( $arrAttributes as $attribute )
 			{
