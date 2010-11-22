9c0024fe7bdce69e1b5d866ffa0913ebb535a3f6
Andreas Schempp <andreas@schempp.ch>
2010-11-22 12:25:35 +0000

    Fixed listing module having a template assigned by default

--- system/modules/isotope/dca/tl_module.php
+++ system/modules/isotope/dca/tl_module.php
@@ -51,7 +51,6 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['iso_relatedproducts']	= '{title_leg
 $GLOBALS['TL_DCA']['tl_module']['fields']['iso_list_layout'] = array
 (
 	'label'						=> &$GLOBALS['TL_LANG']['tl_module']['iso_list_layout'],
-	'default'					=> 'iso_list_default',
 	'exclude'					=> true,
 	'inputType'					=> 'select',
 	'options_callback'			=> array('tl_module_isotope', 'getListTemplates'),
