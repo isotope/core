9c6b358f900f1f1747ee978b96ee816e4c2849bc
Fred Bliss <fred.bliss@intelligentspark.com>
2011-01-28 20:06:20 +0000

    Missing period in Authorize.net Payment Module that has been bugging me on each install :)

--- system/modules/isotope/PaymentAuthorizeDotNet.php
+++ system/modules/isotope/PaymentAuthorizeDotNet.php
@@ -79,7 +79,7 @@ class PaymentAuthorizeDotNet extends IsotopePayment
 		$fieldsFinal = rtrim($fields, '&');
 
 		$objRequest = new Request();
-			$objRequest->send('https://'.($this->debug ? 'test' : 'secure')'.authorize.net/gateway/transact.dll', $fields, 'post');
+			$objRequest->send('https://'.($this->debug ? 'test' : 'secure').'.authorize.net/gateway/transact.dll', $fields, 'post');
 
 		$arrResponses = $this->handleResponse($objRequest->response);
 		$arrResponseCodes = $this->getResponseCodes($objRequest->response);
