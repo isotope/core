5508e043594f0d93974aa7a3580ef538cf0dd059
Andreas Schempp <andreas@schempp.ch>
2011-01-28 13:22:59 +0000

    Fixed text area floating in backend product edit mode

--- system/modules/isotope/dca/tl_iso_products.php
+++ system/modules/isotope/dca/tl_iso_products.php
@@ -1514,6 +1514,11 @@ $strBuffer .= '<th style="text-align:center"><img src="system/themes/default/ima
 				$arrData['eval']['datepicker'] = $this->getDatePickerString();
 			}
 			
+			if ($objAttributes->type == 'textarea' || $objAttributes->rte != '')
+			{
+				$arrData['eval']['tl_class'] = 'clr';
+			}
+
 			// Prepare options
 			if ($objAttributes->foreignKey != '')
 			{
