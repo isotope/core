f9bddc6f4f10e8e079079f748280c2befa988229
Andreas Schempp <andreas@schempp.ch>
2011-02-02 09:31:07 +0000

    Downloaded all translations from betatest

---
+++ languages/fr/tl_iso_payment_modules.php
@@ -0,0 +1,35 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Winans Creative 2009, Intelligent Spark 2010, iserv.ch GmbH 2010
+ * @author     Rebecca Jutzi <rebecca.jutzi@bluewin.ch>
+ * @author     Simon Moos <cc@simonmoos.com>
+ * @author     Cyril Ponce <cyril@contao.fr>
+ * @author     Stéphane Cagni <stephane@cagni.fr>
+ * @author     Katelle Ave <contact@graphikat.net>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['saferpay_accountid'][0] = 'Compte-ID Saferpay';
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['saferpay_accountid'][1] = 'Entrez votre compte-ID unique Saferpay';
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['saferpay_description'][0] = 'Description de la commande';
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['saferpay_description'][1] = 'Le client verra cette description sur la page de paiement Saferpay.';
+
