d1b29f56509431746bdebecfba9c86e9f11ccad4
Andreas Schempp <andreas@schempp.ch>
2011-02-08 14:51:16 +0000

    english language files Payone payment module

---
+++ languages/en/payment.php
@@ -0,0 +1,41 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Andreas Schempp 2011
+ * @author     Andreas Schempp <andreas@schempp.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+
+/**
+ * PAYONE Payment Type
+ */
+$GLOBALS['TL_LANG']['MSG']['payone']['elv']		= 'Debit withdrawal';
+$GLOBALS['TL_LANG']['MSG']['payone']['cc']		= 'Credit card';
+$GLOBALS['TL_LANG']['MSG']['payone']['dc']		= 'Debit card';
+$GLOBALS['TL_LANG']['MSG']['payone']['vor']		= 'Prepayment';
+$GLOBALS['TL_LANG']['MSG']['payone']['rec']		= 'Invoice';
+$GLOBALS['TL_LANG']['MSG']['payone']['sb']		= 'Online bank transfer';
+$GLOBALS['TL_LANG']['MSG']['payone']['wlt']		= 'e-Wallet';
+
