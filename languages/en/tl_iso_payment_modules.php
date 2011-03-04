6c5d6515338c461d0bb35dc9f997466e9a0ec56f
Andreas Schempp <andreas@schempp.ch>
2011-03-04 15:10:24 +0000

    ExperCash payment module (www.expercash.de)

---
+++ languages/en/tl_iso_payment_modules.php
@@ -0,0 +1,29 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * PHP version 5
+ * @copyright  Rispler&Rispler Designer Partnerschaftsgesellschaft 2011
+ * @author     Andreas Schempp <andreas@schempp.ch>
+ * @license    commercial
+ * @version    $Id$
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['expercash_popupId']	= array('ExperCash Popup-ID', 'Geben Sie die Popup-ID aus Ihrem ExperCash Portal ein.');
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['expercash_profile']	= array('ExperCash Profile', 'Geben Sie die dreistellige Profilnummer ein.');
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['expercash_popupKey']	= array('ExperCash Popup-Key', 'Geben Sie den Popup-Key aus Ihrem ExperCash Portal ein.');
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['expercash_css']		= array('CSS-Vorlage', 'Wählen Sie eine CSS-Datei für die Übergabe an ExperCash.');
+
