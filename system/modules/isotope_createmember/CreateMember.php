44d8e990058a75582113c8fdf748334b4ed07e07
Fred Bliss <fred.bliss@intelligentspark.com>
2011-04-06 17:31:22 +0000

    iso_createmember revisions. Need to add configurable options into the checkout module dca.

--- system/modules/isotope_createmember/CreateMember.php
+++ system/modules/isotope_createmember/CreateMember.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -29,51 +29,69 @@
 class CreateMember extends Frontend
 {
 
-	public function addMember($objOrder, $objCart)
+	public function addMember($orderId, $blnCheckout=false, $objModule)
 	{
-		$arrData = array();
-
-		//Get order info and email address
-		$arrBilling = $objOrder->billing_address;
-
-		//First check for existing user email. Don't want to duplicate.
-		$objUser = $this->Database->prepare("SELECT * FROM tl_member WHERE email=?")->execute($arrBilling['email']);
-
-		if (!$objUser->numRows)
+		if($blnCheckout)
 		{
-			//Only small difference here. Perhaps work to
-			$arrData = array
-				(
-					'firstname' 	=> ($arrBilling['firstname'] ? $arrBilling['firstname'] : 'newuser'.$objOrder->id),
-					'lastname'		=> ($arrBilling['lastname'] ? $arrBilling['lastname'] : 'newuser'.$objOrder->id),
-					'street'		=> $arrBilling['street_1'],
-					'city'			=> $arrBilling['city'],
-					'state'			=> $arrBilling['subdivision'],
-					'postal'		=> $arrBilling['postal'],
-					'country'		=> $arrBilling['country'],
-					'email'			=> ($arrBilling['email'] ? $arrBilling['email'] : $objOrder->id.'@snacktaxi.com'),
-					'phone'			=> $arrBilling['phone'],
-					'username'		=> $arrBilling['lastname'] . time(),
-					'password'		=> $this->createRandomPassword()
-				);
-
-			$this->createNewMember($arrData);
+			$arrData = array();
+			$objOrder = $this->Database->prepare("SELECT * FROM tl_iso_orders WHERE id=?")->execute($orderId);
+			
+			//Get order info and email address
+			$arrBilling = deserialize($objOrder->billing_address);
+						
+			if($arrBilling['saveUserInfo'])
+			{
+			
+				//First check for existing user email. Don't want to duplicate.
+				$objUser = $this->Database->prepare("SELECT * FROM tl_member WHERE email=?")->execute($arrBilling['email']);
+				if(!$objUser->numRows)
+				{		
+					$strSalt = substr(md5(uniqid('', true)), 0, 23);
+					$clearPass = $this->createRandomPassword();
+					$hashPass = sha1($strSalt . $clearPass) . ':' . $strSalt;
+					
+					//Only small difference here. Perhaps work to 
+					$arrData = array
+					(
+						'firstname' 	=> ($arrBilling['firstname'] ? $arrBilling['firstname'] : 'newuser'.$objOrder->id),
+						'lastname'		=> ($arrBilling['lastname'] ? $arrBilling['lastname'] : 'newuser'.$objOrder->id),
+						'street'		=> $arrBilling['street'],
+						'city'			=> $arrBilling['city'],
+						'state'			=> $arrBilling['state'],
+						'postal'		=> $arrBilling['postal'],
+						'country'		=> $arrBilling['country'],
+						'email'			=> ($arrBilling['email'] ? $arrBilling['email'] : $objOrder->id.'@ibuycoffee.com'),
+						'phone'			=> $arrBilling['phone'],
+						'username'		=> $arrBilling['email'],
+						'password'		=> $hashPass
+					);
+					
+										
+					if($objModule->arrOrderData['form_email_newsletter']>0)
+					{
+						$arrData['signup'] = true;
+					}
+					
+					$this->createNewMember($arrData,$orderId,$clearPass);
+				}
+			}
+			
+			return true;
 		}
-
-		return true;
+		return $blnCheckout;
 	}
-
+	
 	/**
 	 * Create a new member
 	 * @param array
 	 */
-	protected function createNewMember($arrData)
+	protected function createNewMember($arrData, $orderId, $clearPass)
 	{
 		$arrData['tstamp'] = time();
 		$arrData['login'] = 1;
 		$arrData['activation'] = md5(uniqid(mt_rand(), true));
 		$arrData['dateAdded'] = $arrData['tstamp'];
-
+		
 		//Find Customers Group... if doesn't exist, create one.
 		$arrGroup = array();
 		$objGroup = $this->Database->execute("SELECT * FROM tl_member_group WHERE name='Customers'");
@@ -87,39 +105,19 @@ class CreateMember extends Frontend
 			{
 				return;
 			}
+			
 		}
 		else
 		{
 			$arrGroup[] = $objGroup->id;
 		}
-
+		
 		// Set default group
 		$arrData['groups'] = serialize($arrGroup);
 
 		// Auto-activate account
 		$arrData['disable'] = 0;
-
-		//Find Customers Newsletter... if doesn't exist, create one.
-		$arrNews = array();
-		$objNews = $this->Database->execute("SELECT * FROM tl_newsletter_channel WHERE title='General Contact'");
-		if(!$objNews->numRows)
-		{
-			try
-			{
-				$arrNews[] = $this->Database->execute("INSERT INTO tl_newsletter_channel (title) VALUES ('General Contact')")->insertID;
-			}
-			catch (Exception $e)
-			{
-				return;
-			}
-		}
-		else
-		{
-			$arrNews[] = $objNews->id;
-		}
-
-		$arrData['newsletter'] = serialize($arrNews);
-
+				
 		try
 		{
 			// Create user
@@ -128,8 +126,11 @@ class CreateMember extends Frontend
 		}
 		catch (Exception $e)
 		{
+			$this->log('Create User Failed On Order ' . $orderId . '.', 'ModuleIsotopeCheckout writeOrder()', TL_ERROR);
 			return;
 		}
+		
+		$this->Database->prepare("UPDATE tl_iso_orders SET pid=? WHERE id=?")->execute($insertId, $orderId);
 
 		// HOOK: send insert ID and user data
 		if (isset($GLOBALS['TL_HOOKS']['createNewUser']) && is_array($GLOBALS['TL_HOOKS']['createNewUser']))
@@ -140,33 +141,47 @@ class CreateMember extends Frontend
 				$this->$callback[0]->$callback[1]($insertId, $arrData);
 			}
 		}
+		
+		
+		$strConfirmation = 'Thank you for your registration on '.$this->Environment->host.'.'. "\n\n";
+		$strConfirmation .= 'Your username is: '.$arrData['username']." \n";
+		$strConfirmation .= 'and your password is: '.$clearPass."\n\n";
+		$strConfirmation .= 'Please keep this info in a safe place and use it to re-login to your account. '. "\n";
+				
+		$objEmail = new Email();
+
+		$objEmail->from = 'sales@ibuycoffee.com';
+		$objEmail->fromName = $GLOBALS['TL_ADMIN_NAME'];
+		$objEmail->subject = sprintf($GLOBALS['TL_LANG']['MSC']['emailSubject'], $this->Environment->host);
+		$objEmail->text = $strConfirmation;
+		$objEmail->sendTo($arrData['email']);
 
 	}
-
-	/**
-	 * The letter l (lowercase L) and the number 1
-	 * have been removed, as they can be mistaken
-	 * for each other.
+	
+	/** 
+	 * The letter l (lowercase L) and the number 1 
+	 * have been removed, as they can be mistaken 
+	 * for each other. 
 	 * From http://www.totallyphp.co.uk/code/create_a_random_password.htm
-	 */
-
-	protected function createRandomPassword() {
-
-	    $chars = "abcdefghijkmnopqrstuvwxyz023456789";
-	    srand((double)microtime()*1000000);
-	    $i = 0;
-	    $pass = '' ;
-
-	    while ($i <= 7) {
-	        $num = rand() % 33;
-	        $tmp = substr($chars, $num, 1);
-	        $pass = $pass . $tmp;
-	        $i++;
+	 */ 
+	
+	protected function createRandomPassword() { 
+	
+	    $chars = "abcdefghijkmnopqrstuvwxyz023456789"; 
+	    srand((double)microtime()*1000000); 
+	    $i = 0; 
+	    $pass = '' ; 
+	
+	    while ($i <= 7) { 
+	        $num = rand() % 33; 
+	        $tmp = substr($chars, $num, 1); 
+	        $pass = $pass . $tmp; 
+	        $i++; 
 	    }
-
-	    return $pass;
-
-	}
+	
+	    return $pass; 
+	
+	} 
 
 
 }
--- system/modules/isotope_createmember/config/config.php
+++ system/modules/isotope_createmember/config/config.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -33,5 +33,5 @@
 /**
  * Hooks
  */
-$GLOBALS['ISO_HOOKS']['checkout'][] = array('CreateMember','addMember');
-
+$GLOBALS['TL_HOOKS']['iso_writeOrder'][] = array('CreateMember','addMember');
+?>
