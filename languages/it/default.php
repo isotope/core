a9a695f966b702b6abd27b3268cbd097c045553d
Andreas Schempp <andreas@schempp.ch>
2011-05-18 12:24:28 +0000

    Downloaded latest language files from betatest

---
+++ languages/it/default.php
@@ -0,0 +1,37 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  2009-2011 Isotope eCommerce Workgroup
+ * @author     Blair Winans <blair@winanscreative.com>
+ * @author     Angelica Schempp <aschempp@gmx.net>
+ * @author     Paolo B. <paolob@contaocms.it>
+ * @author     Dan N <dan@dss.uniud.it>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+$GLOBALS['TL_LANG']['PAY']['saferpay'][0] = 'Saferpay';
+$GLOBALS['TL_LANG']['MSC']['pay_with_saferpay'][0] = 'Paga con Saferpay';
+$GLOBALS['TL_LANG']['MSC']['pay_with_saferpay'][1] = 'Sarà reindirizzato al sito web di Saferway. Se non è automaticamente indirizzato, per cortesia faccia click sul collegamento "Paga adesso"';
+$GLOBALS['TL_LANG']['MSC']['pay_with_saferpay'][2] = 'Paga adesso';
+
---
+++ languages/it/tl_iso_payment_modules.php
@@ -0,0 +1,37 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  2009-2011 Isotope eCommerce Workgroup
+ * @author     Blair Winans <blair@winanscreative.com>
+ * @author     Angelica Schempp <aschempp@gmx.net>
+ * @author     Paolo B. <paolob@contaocms.it>
+ * @author     Dan N <dan@dss.uniud.it>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['saferpay_accountid'][0] = 'ID Conto Saferpay';
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['saferpay_accountid'][1] = 'Per cortesia inserisca il suo ID unico per il conto Saferpay';
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['saferpay_description'][0] = 'Descizione checkout';
+$GLOBALS['TL_LANG']['tl_iso_payment_modules']['saferpay_description'][1] = 'Il cliente vedrà questa descrizione nella pagina checkout di Saferpay.';
+
