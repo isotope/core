1a3a5eaacca3e296605971def960bb5038e45225
Andreas Schempp <andreas@schempp.ch>
2011-05-18 12:24:28 +0000

    Downloaded latest language files from betatest

---
+++ system/modules/isotope_multilingual/languages/it/default.php
@@ -0,0 +1,40 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  2009-2011 Isotope eCommerce Workgroup
+ * @author     Blair Winans <blair@winanscreative.com>
+ * @author     Angelica Schempp <aschempp@gmx.net>
+ * @author     Paolo B. <paolob@contaocms.it>
+ * @author     Dan N <dan@dss.uniud.it>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+$GLOBALS['TL_LANG']['MSC']['translationSaved'] = 'Le sue modifiche sono state cambiate.';
+$GLOBALS['TL_LANG']['MSC']['translationEdit'] = 'Modifichi il file %s dall\'estensione "%s"';
+$GLOBALS['TL_LANG']['MSC']['translationSelect'] = 'Per cortesia selezioni un\'estensione e un file.';
+$GLOBALS['TL_LANG']['MSC']['translationError'] = 'E\' stato impossibile analizzare Il file destinazione';
+$GLOBALS['TL_LANG']['MSC']['translationErrorSource'] = 'E\' stato impossibile analizzare Il file sorgente';
+$GLOBALS['TL_LANG']['MSC']['translationDiffHeadline'] = 'Cambiamenti tra "%s/languages/%s/%s" su localhost e Isotope SVN.';
+$GLOBALS['TL_LANG']['MSC']['translationSVNError'] = 'Impossibile recuperare il file da SVN.';
+
