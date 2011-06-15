e18a564c2cd1dc3311eea4d0ad5a93f4bbfb3b66
Andreas Schempp <andreas@schempp.ch>
2011-06-15 15:43:23 +0000

    includeBlankOption=true for select options using foreignKey (Ticket #292)

--- system/modules/isotope/dca/tl_iso_products.php
+++ system/modules/isotope/dca/tl_iso_products.php
@@ -1528,6 +1528,7 @@ $strBuffer .= '<th style="text-align:center"><img src="system/themes/default/ima
 			if ($objAttributes->foreignKey != '')
 			{
 				$arrData['foreignKey'] = $objAttributes->foreignKey;
+				$arrData['eval']['includeBlankOption'] = true;
 				unset($arrData['options']);
 			}
 			else
