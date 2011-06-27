45504fb18d280fe2311907e59de0655d76ae2f71
Andreas Schempp <andreas@schempp.ch>
2011-06-27 13:23:07 +0000

    First draft of payment module for "Sparkasse" germany. This is not yet functional!

---
+++ config/database.sql
@@ -0,0 +1,19 @@
+-- **********************************************************
+-- *                                                        *
+-- * IMPORTANT NOTE                                         *
+-- *                                                        *
+-- * Do not import this file manually but use the TYPOlight *
+-- * install tool to create and maintain database tables!   *
+-- *                                                        *
+-- **********************************************************
+
+-- 
+-- Table `tl_iso_payment_modules`
+-- 
+
+CREATE TABLE `tl_iso_payment_modules` (
+  `sparkasse_paymentmethod` varchar(32) NOT NULL default '',
+  `sparkasse_sslmerchant` varchar(16) NOT NULL default '',
+  `sparkasse_sslpassword` varchar(255) NOT NULL default '',
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
