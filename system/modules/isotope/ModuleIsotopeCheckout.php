8192db927dc676c97a8c72464767ece8bc00806e
Andreas Schempp <andreas@schempp.ch>
2011-07-06 07:44:33 +0000

    Fixed text email fields, make sure they never contain HTML

--- system/modules/isotope/ModuleIsotopeCheckout.php
+++ system/modules/isotope/ModuleIsotopeCheckout.php
@@ -406,7 +406,7 @@ class ModuleIsotopeCheckout extends ModuleIsotope
 			$strBillingAddress = $this->Isotope->generateAddressString($this->Isotope->Cart->billingAddress, $this->Isotope->Config->billing_fields);
 
 			$this->arrOrderData['billing_address']		= $strBillingAddress;
-			$this->arrOrderData['billing_address_text']	= str_replace('<br />', "\n", $strBillingAddress);
+			$this->arrOrderData['billing_address_text']	= strip_tags(str_replace(array('<br />', '<br>'), "\n", $strBillingAddress));
 		}
 
 		return $objTemplate->parse();
@@ -982,7 +982,7 @@ class ModuleIsotopeCheckout extends ModuleIsotope
 			'shippingPrice'				=> $this->Isotope->formatPriceWithCurrency($this->Isotope->Cart->Shipping->price, false),
 			'paymentPrice'				=> $this->Isotope->formatPriceWithCurrency($this->Isotope->Cart->Payment->price, false),
 			'grandTotal'				=> $this->Isotope->formatPriceWithCurrency($this->Isotope->Cart->grandTotal, false),
-			'cart_text'					=> $this->replaceInsertTags($this->Isotope->Cart->getProducts('iso_products_text')),
+			'cart_text'					=> strip_tags($this->replaceInsertTags($this->Isotope->Cart->getProducts('iso_products_text'))),
 			'cart_html'					=> $this->replaceInsertTags($this->Isotope->Cart->getProducts('iso_products_html')),
 		));
 
