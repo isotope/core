d24a6e4c6d94a63e781ec72470f7b742cc01740b
Andreas Schempp <andreas@schempp.ch>
2011-09-29 12:10:31 +0000

    Code cleaning with regex search-replace. Removed unnecessary white-spaces and tabs. According to Contao coding style, empty lines should not contain any characters.

--- config/config.php
+++ config/config.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -23,7 +23,7 @@
  * PHP version 5
  * @copyright  Isotope eCommerce Workgroup 2011
  * @author     Andreas Schempp <andreas@schempp.ch>
- * @author     Kamil Kuźmiński <kamil.kuzminski@gmail.com> 
+ * @author     Kamil Kuźmiński <kamil.kuzminski@gmail.com>
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  * @version    $Id$
  */
