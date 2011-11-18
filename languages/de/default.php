7a3080f0bc611f85c612c7849572dcd7f560bba2
Andreas Schempp <andreas@schempp.ch>
2011-11-18 16:47:03 +0000

    Sync language files from FTP to SVN

---
+++ languages/de/default.php
@@ -0,0 +1,41 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  2009-2011 Isotope eCommerce Workgroup
+ * @author     Andreas Burg <ab@andreasburg.de>
+ * @author     Nikolas Runde <info@nrmedia.de>
+ * @author     Patrick Grob <grob@a-sign.ch>
+ * @author     Frank Berger <berger@mediastuff.de>
+ * @author     Angelica Schempp <aschempp@gmx.net>
+ * @author     Oliver Hoff <oliver@hoff.com>
+ * @author     Stefan Preiss <stefan@preiss-at-work.de>
+ * @author     Nina Gerling <gerling@ena-webstudio.de>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+$GLOBALS['ISO_LANG']['PAY']['expercash'][0] = 'ExperCash';
+$GLOBALS['TL_LANG']['MSC']['pay_with_redirect'][0] = 'Zahlung ausführen';
+$GLOBALS['TL_LANG']['MSC']['pay_with_redirect'][1] = 'Sie werden nun zum Zahlunganbieter weitergeleitet. Falls die Weiterleitung nicht automatisch funktioniert, klicken Sie auf "Bezahlen".';
+$GLOBALS['TL_LANG']['MSC']['pay_with_redirect'][2] = 'Bezahlen';
+
