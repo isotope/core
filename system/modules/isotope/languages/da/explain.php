b47aed520a7c87c57ee7ff7f8d300991d50f06e6
Andreas Schempp <andreas@schempp.ch>
2011-11-18 16:47:03 +0000

    Sync language files from FTP to SVN

---
+++ system/modules/isotope/languages/da/explain.php
@@ -0,0 +1,44 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  2009-2011 Isotope eCommerce Workgroup
+ * @author     Stefan Johannsen <stefan@reklamehuset.dk>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][0][0] = '##ordre_id##';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][0][1] = 'Det unikke nummer for denne ordre';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][1][0] = '##emner##';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][1][1] = 'Antal emner i kurven';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][2][0] = '##produkter##';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][2][1] = 'Varer i kurven';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][3][0] = '##subTotal##';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][3][1] = 'Ordrens subtotal';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][4][0] = '##taxTotal##';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][4][1] = 'Momsbeløb (ekskl. forsendelse)';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][5][0] = '##ForsendelsesGebyr##';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][5][1] = 'Forsendelsesgebyr';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][6][0] = '##Total##';
+$GLOBALS['TL_LANG']['XPL']['isoMailTokens'][6][1] = 'Total';
+
---
+++ system/modules/isotope/languages/da/tl_iso_config.php
@@ -0,0 +1,73 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  2009-2011 Isotope eCommerce Workgroup
+ * @author     Stefan Johannsen <stefan@reklamehuset.dk>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+$GLOBALS['TL_LANG']['tl_iso_config']['name'][0] = 'Navn på indstillingen';
+$GLOBALS['TL_LANG']['tl_iso_config']['name'][1] = 'Indtast det navn du ønsker at give din indstilling';
+$GLOBALS['TL_LANG']['tl_iso_config']['label'][0] = 'Mærkat';
+$GLOBALS['TL_LANG']['tl_iso_config']['label'][1] = 'Mærkaten bruges i shoppen, f.eks. til at skifte indstillinger med.';
+$GLOBALS['TL_LANG']['tl_iso_config']['fallback'][0] = 'Sæt som standard butik';
+$GLOBALS['TL_LANG']['tl_iso_config']['missing_image_placeholder'][0] = 'Erstatningsbillede';
+$GLOBALS['TL_LANG']['tl_iso_config']['missing_image_placeholder'][1] = 'Dette billede vil blive brugt hvis der mangler et billede til et produkt.';
+$GLOBALS['TL_LANG']['tl_iso_config']['priceCalculateFactor'][0] = 'Pris faktor';
+$GLOBALS['TL_LANG']['tl_iso_config']['priceCalculateFactor'][1] = 'Standard er 1. Du kan bruge denne faktor til at konvertere mellem forskellige valutaer.';
+$GLOBALS['TL_LANG']['tl_iso_config']['priceCalculateMode'][0] = 'Beregningsmetode';
+$GLOBALS['TL_LANG']['tl_iso_config']['priceCalculateMode'][1] = 'Divider eller multiplicer med denne faktor';
+$GLOBALS['TL_LANG']['tl_iso_config']['priceRoundPrecision'][0] = 'Afrundningsforhold';
+$GLOBALS['TL_LANG']['tl_iso_config']['priceRoundPrecision'][1] = 'Hvor mange decimaler skal din afrundning indeholde. Du bør vælge en værdi mellem 0 og 2 for at alle betalingsgateways virker korrekt. Se evt. PHP manual for round().';
+$GLOBALS['TL_LANG']['tl_iso_config']['currency'][0] = 'Valuta';
+$GLOBALS['TL_LANG']['tl_iso_config']['currency'][1] = 'Vælg venligst en valuta for denne butik';
+$GLOBALS['TL_LANG']['tl_iso_config']['currencySymbol'][0] = 'Brug valutasymbol';
+$GLOBALS['TL_LANG']['tl_iso_config']['currencySymbol'][1] = 'Brug et valutasymbol ($, €, DKK) hvis tilgængeligt.';
+$GLOBALS['TL_LANG']['tl_iso_config']['currencySpace'][0] = 'Medtag mellemrum';
+$GLOBALS['TL_LANG']['tl_iso_config']['currencySpace'][1] = 'Tilføjer luft mellem valutasymbol og prisen';
+$GLOBALS['TL_LANG']['tl_iso_config']['currencyPosition'][0] = 'Position af valutasymbol';
+$GLOBALS['TL_LANG']['tl_iso_config']['currencyPosition'][1] = 'Vælg om du ønsker valutasymbolet vist til højre eller venstre for prisen.';
+$GLOBALS['TL_LANG']['tl_iso_config']['currencyFormat'][0] = 'Valuta formatering';
+$GLOBALS['TL_LANG']['tl_iso_config']['currencyFormat'][1] = 'Vælg en formatering for priser';
+$GLOBALS['TL_LANG']['tl_iso_config']['invoiceLogo'][0] = 'Faktura logo';
+$GLOBALS['TL_LANG']['tl_iso_config']['invoiceLogo'][1] = 'Vælg et logo der skal vises på fakturaer';
+$GLOBALS['TL_LANG']['tl_iso_config']['firstname'][0] = 'Fornavn';
+$GLOBALS['TL_LANG']['tl_iso_config']['firstname'][1] = 'Indtast dit fornavn';
+$GLOBALS['TL_LANG']['tl_iso_config']['lastname'][0] = 'Efternavn';
+$GLOBALS['TL_LANG']['tl_iso_config']['lastname'][1] = 'Indtast dit efternavn';
+$GLOBALS['TL_LANG']['tl_iso_config']['company'][0] = 'Firmanavn';
+$GLOBALS['TL_LANG']['tl_iso_config']['company'][1] = 'Indtast dit firmanavn';
+$GLOBALS['TL_LANG']['tl_iso_config']['street_1'][0] = 'Adresse';
+$GLOBALS['TL_LANG']['tl_iso_config']['street_1'][1] = 'Indtast din adresse';
+$GLOBALS['TL_LANG']['tl_iso_config']['street_2'][0] = 'Adresse 2';
+$GLOBALS['TL_LANG']['tl_iso_config']['street_3'][0] = 'Adresse 3';
+$GLOBALS['TL_LANG']['tl_iso_config']['postal'][0] = 'Postnummer';
+$GLOBALS['TL_LANG']['tl_iso_config']['city'][0] = 'By';
+$GLOBALS['TL_LANG']['tl_iso_config']['subdivision'][0] = 'Region';
+$GLOBALS['TL_LANG']['tl_iso_config']['country'][0] = 'Land';
+$GLOBALS['TL_LANG']['tl_iso_config']['phone'][0] = 'Telefonnummer';
+$GLOBALS['TL_LANG']['tl_iso_config']['emailShipping'][0] = 'E-mail';
+$GLOBALS['TL_LANG']['tl_iso_config']['shipping_countries'][0] = 'Fragt til disse lande';
+$GLOBALS['TL_LANG']['tl_iso_config']['shipping_countries'][1] = 'Vælg de lande du ønsker skal være tilgængelige for forsendelse.';
+
