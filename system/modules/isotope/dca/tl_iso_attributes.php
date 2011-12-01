3ea14a62fd9a305d05453b44a9d5eeb58870055a
Andreas Schempp <andreas@schempp.ch>
2011-12-01 19:30:59 +0000

    Do not clean subpalette fields

--- system/modules/isotope/dca/tl_iso_attributes.php
+++ system/modules/isotope/dca/tl_iso_attributes.php
@@ -480,8 +480,9 @@ class tl_iso_attributes extends Backend
 
 		$arrFields = array_keys($GLOBALS['TL_DCA']['tl_iso_attributes']['fields']);
 		$arrKeep = trimsplit(',|;', $strPalette);
+		$arrSubpalettes = trimsplit(',', implode(',', (array)$GLOBALS['TL_DCA']['tl_iso_attributes']['subpalettes']));
 
-		$arrClean = array_diff($arrFields, $arrKeep, array('pid', 'sorting'));
+		$arrClean = array_diff($arrFields, $arrKeep, $arrSubpalettes, array('pid', 'sorting'));
 
 		$this->Database->execute("UPDATE tl_iso_attributes SET " . implode("='', ", $arrClean) . "='' WHERE id={$dc->id}");
 	}
