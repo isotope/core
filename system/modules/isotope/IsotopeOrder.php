97077fc842df302484cea2f73b0d4d46d8e558b8
Andreas Schempp <andreas@schempp.ch>
2011-12-20 13:00:23 +0000

    Numberic order ID didn't get incremented (#2222)

--- system/modules/isotope/IsotopeOrder.php
+++ system/modules/isotope/IsotopeOrder.php
@@ -377,16 +377,17 @@ class IsotopeOrder extends IsotopeProductCollection
 		}
 
 		$strPrefix = $this->Isotope->Config->orderPrefix;
+		$intPrefix = utf8_strlen($strPrefix);
 		$arrConfigIds = $this->Database->execute("SELECT id FROM tl_iso_config WHERE store_id=" . $this->Isotope->Config->store_id)->fetchEach('id');
 
 		// Lock tables so no other order can get the same ID
 		$this->Database->lockTables(array('tl_iso_orders'));
 
 		// Retrieve the highest available order ID
-		$objMax = $this->Database->prepare("SELECT order_id FROM tl_iso_orders WHERE order_id LIKE '$strPrefix%' AND config_id IN (" . implode(',', $arrConfigIds) . ") ORDER BY order_id DESC")->limit(1)->executeUncached();
-		$intMax = (int) substr($objMax->order_id, strlen($strPrefix));
+		$objMax = $this->Database->prepare("SELECT order_id FROM tl_iso_orders WHERE " . ($strPrefix != '' ? "order_id LIKE '$strPrefix%' AND " : '') . "config_id IN (" . implode(',', $arrConfigIds) . ") ORDER BY CAST(" . ($strPrefix != '' ? "SUBSTRING(order_id, $intPrefix)" : 'order_id') . " AS UNSIGNED) DESC")->limit(1)->executeUncached();
+		$intMax = (int) substr($objMax->order_id, $intPrefix);
+		
 		$this->strOrderId = $strPrefix . str_pad($intMax+1, $this->Isotope->Config->orderDigits, '0', STR_PAD_LEFT);
-
 		$this->Database->query("UPDATE tl_iso_orders SET order_id='{$this->strOrderId}' WHERE id={$this->id}");
 		$this->Database->unlockTables();
 
