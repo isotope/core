6128ac6cab33101d91ade44348b0e61e94859c2f
Andreas Schempp <andreas@schempp.ch>
2012-02-27 19:05:35 +0100

    Updated copyright statement for isotope_multilingual

--- system/modules/isotope_multilingual/dca/tl_user.php
+++ system/modules/isotope_multilingual/dca/tl_user.php
@@ -19,7 +19,7 @@
  * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  Andreas Schempp 2010
+ * @copyright  Isotope eCommerce Workgroup 2009-2012
  * @author     Andreas Schempp <andreas@schempp.ch>
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
