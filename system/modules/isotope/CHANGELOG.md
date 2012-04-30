37f6330e9a9f23e21e119653c5b7505ddd9bf602
Andreas Schempp <andreas@schempp.ch>
2012-04-30 09:37:28 +0200

    Advanced pricing did not work on product variants (#3052)

--- system/modules/isotope/CHANGELOG.md
+++ system/modules/isotope/CHANGELOG.md
@@ -16,6 +16,7 @@ Version 1.3.1 stable (????-??-??)
 - the filter template did not have a button label on javascript fallback (#3113)
 - potential exception on product sorting view in the backend (#3094)
 - The inherit wizard caused javascript errors when no tooltip was given on a field (#3092)
+- Advanced pricing did not work on product variants (#3052)
 
 Version 1.3.0 stable (2012-04-04)
 ---------------------------------
--- system/modules/isotope/IsotopeProduct.php
+++ system/modules/isotope/IsotopeProduct.php
@@ -1152,7 +1152,7 @@ class IsotopeProduct extends Controller
 															AND member_group IN(" . ((FE_USER_LOGGED_IN === true && count($this->User->groups)) ? (implode(',', $this->User->groups) . ',') : '') . "0)
 															AND (start='' OR start<$time)
 															AND (stop='' OR stop>$time)
-															AND pid=" . ($this->pid ? $this->pid : $this->id) . "
+															AND pid=" . (($this->pid > 0 && !in_array('price', $this->arrVariantAttributes)) ? $this->pid : $this->id) . "
 														ORDER BY config_id DESC, " . ((FE_USER_LOGGED_IN === true && count($this->User->groups)) ? ('member_group=' . implode(' DESC, member_group=', $this->User->groups) . ' DESC') : 'member_group DESC') . ", start DESC, stop DESC
 														LIMIT 1
 													)
