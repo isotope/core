1f6bc06f78d538b9e583d8cfa4783db582a5dc6d
Andreas Schempp <andreas.schempp@terminal42.ch>
2012-10-25 12:15:38 +0200

    Datepicker (and custom wizards) now work in the front end too

--- system/modules/isotope/IsotopeProduct.php
+++ system/modules/isotope/IsotopeProduct.php
@@ -1268,12 +1268,65 @@ class IsotopeProduct extends Controller
 			}
 		}
 
+		$wizard = '';
+
+		// Datepicker
+		if ($arrData['eval']['datepicker'])
+		{
+			$GLOBALS['TL_JAVASCRIPT'][] = 'plugins/datepicker/datepicker.js';
+			$GLOBALS['TL_CSS'][] = 'plugins/datepicker/dashboard.css';
+
+			$rgxp = $arrData['eval']['rgxp'];
+			$format = Date::formatToJs($GLOBALS['TL_CONFIG'][$rgxp.'Format']);
+
+			switch ($rgxp)
+			{
+				case 'datim':
+					$time = ",\n      timePicker:true";
+					break;
+
+				case 'time':
+					$time = ",\n      pickOnly:\"time\"";
+					break;
+
+				default:
+					$time = '';
+					break;
+			}
+
+			$wizard .= ' <img src="plugins/datepicker/icon.gif" width="20" height="20" alt="" id="toggle_' . $objWidget->id . '" style="vertical-align:-6px">
+  <script>
+  window.addEvent("domready", function() {
+    new Picker.Date($$("#ctrl_' . $objWidget->id . '"), {
+      draggable:false,
+      toggle:$$("#toggle_' . $objWidget->id . '"),
+      format:"' . $format . '",
+      positionOffset:{x:-197,y:-182}' . $time . ',
+      pickerClass:"datepicker_dashboard",
+      useFadeInOut:!Browser.ie,
+      startDay:' . $GLOBALS['TL_LANG']['MSC']['weekOffset'] . ',
+      titleFormat:"' . $GLOBALS['TL_LANG']['MSC']['titleFormat'] . '"
+    });
+  });
+  </script>';
+		}
+
+		// Add a custom wizard
+		if (is_array($arrData['wizard']))
+		{
+			foreach ($arrData['wizard'] as $callback)
+			{
+				$this->import($callback[0]);
+				$wizard .= $this->$callback[0]->$callback[1]($this);
+			}
+		}
+
 		if ($objWidget instanceof uploadable)
 		{
 			$this->hasUpload = true;
 		}
 
-		return $objWidget->parse();
+		return $objWidget->parse() . $wizard;
 	}
 
 
