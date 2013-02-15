4ba5a34d2b8fadcf93a75700fcc178f2ab579326
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-02-15 16:22:29 +0100

    Removed author notice, it destroys the diff

--- system/modules/isotope/languages/en/subdivisions.php
+++ system/modules/isotope/languages/en/subdivisions.php
@@ -1286,10 +1286,6 @@ $GLOBALS['TL_LANG']['DIV']['gh']['GH-UW'] = 'Upper West';
 $GLOBALS['TL_LANG']['DIV']['gh']['GH-TV'] = 'Volta';
 $GLOBALS['TL_LANG']['DIV']['gh']['GH-WP'] = 'Western';
 
-/**
- * UK Countries
- * @author    Ben Kenyon <ben@360fusion.co.uk>
- */
 $GLOBALS['TL_LANG']['DIV']['gi']['GI-GI'] = 'Gibraltar';
 $GLOBALS['TL_LANG']['DIV']['gb']['GB-CHA'] = 'Channel Islands';
 $GLOBALS['TL_LANG']['DIV']['gb']['GB-GSY'] = 'Guernsey';
