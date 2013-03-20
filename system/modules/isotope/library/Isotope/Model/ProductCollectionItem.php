685451b9527d43a384f47f126ac6509a6200075d
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-03-20 17:08:57 +0100

    Cache product in product collection item

--- system/modules/isotope/library/Isotope/Model/ProductCollectionItem.php
+++ system/modules/isotope/library/Isotope/Model/ProductCollectionItem.php
@@ -30,6 +30,12 @@ class ProductCollectionItem extends \Model
      */
     protected static $strTable = 'tl_iso_product_collection_item';
 
+    /**
+     * Cache the current product
+     * @var IsotopeProduct
+     */
+    protected $objProduct;
+
 
     /**
      * Get the product related to this item
@@ -37,26 +43,29 @@ class ProductCollectionItem extends \Model
      */
     public function getProduct()
     {
-        $strClass = $GLOBALS['ISO_PRODUCT'][$this->type]['class'];
+        if (null === $this->objProduct) {
 
-        if ($strClass == '' || class_exists($strClass)) {
-            $strClass = 'Isotope\Product\Standard';
+            $strClass = $GLOBALS['ISO_PRODUCT'][$this->type]['class'];
+
+            if ($strClass == '' || class_exists($strClass)) {
+                $strClass = 'Isotope\Product\Standard';
+            }
+
+            $arrData = array('sku'=>$this->sku, 'name'=>$this->name, 'price'=>$this->price, 'tax_free_price'=>$this->tax_free_price);
+
+            $objProductData = \Database::getInstance()->prepare($strClass::getSelectStatement() . " WHERE p1.language='' AND p1.id=?")
+                                                      ->execute($this->product_id);
+
+            if ($objProductData->numRows) {
+                $arrData = $this->blnLocked ? array_merge($objProductData->row(), $arrData) : $objProductData->row();
+            }
+
+            $this->objProduct = new $strClass($arrData, deserialize($this->options), $this->blnLocked, $this->quantity);
+            $this->objProduct->collection_id = $this->id;
+            $this->objProduct->tax_id = $this->tax_id;
+            $this->objProduct->reader_jumpTo_Override = $this->href_reader;
         }
 
-        $arrData = array('sku'=>$this->sku, 'name'=>$this->name, 'price'=>$this->price, 'tax_free_price'=>$this->tax_free_price);
-
-        $objProductData = \Database::getInstance()->prepare($strClass::getSelectStatement() . " WHERE p1.language='' AND p1.id=?")
-                                                  ->execute($this->product_id);
-
-        if ($objProductData->numRows) {
-            $arrData = $this->blnLocked ? array_merge($objProductData->row(), $arrData) : $objProductData->row();
-        }
-
-        $objProduct = new $strClass($arrData, deserialize($this->options), $this->blnLocked, $this->quantity);
-        $objProduct->collection_id = $this->id;
-        $objProduct->tax_id = $this->tax_id;
-        $objProduct->reader_jumpTo_Override = $this->href_reader;
-
-        return $objProduct;
+        return $this->objProduct;
     }
 }
