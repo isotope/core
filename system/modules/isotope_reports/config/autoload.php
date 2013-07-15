41832ad5a656883819a501c4c5e55c45bbc26f2e
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-07-15 23:15:51 +0200

    Added Contao 3 autoload files

---
+++ system/modules/isotope_reports/config/autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Isotope eCommerce for Contao Open Source CMS
+ *
+ * Copyright (C) 2009-2012 Isotope eCommerce Workgroup
+ *
+ * @package    Isotope
+ * @link       http://www.isotopeecommerce.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register PSR-0 namespace
+ */
+NamespaceClassLoader::add('Isotope', 'system/modules/isotope_reports/library');
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+    'be_iso_reports'                => 'system/modules/isotope_reports/templates',
+    'iso_report_default'            => 'system/modules/isotope_reports/templates',
+));
