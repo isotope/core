86a308c5e168779c61d5e7d4119c44e262fc8d6e
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-07-17 19:43:24 +0200

    Typo

--- system/modules/isotope/dca/tl_iso_payment_modules.php
+++ system/modules/isotope/dca/tl_iso_payment_modules.php
@@ -166,7 +166,7 @@ $GLOBALS['TL_DCA']['tl_iso_payment_modules'] = array
             'label'                 => &$GLOBALS['TL_LANG']['tl_iso_payment_modules']['label'],
             'exclude'               => true,
             'inputType'             => 'text',
-            'eval'                  => array('maxlength'=>255, 'tl_class'=>'w50'),,
+            'eval'                  => array('maxlength'=>255, 'tl_class'=>'w50'),
             'sql'                   => "varchar(255) NOT NULL default ''"
         ),
         'type' => array
