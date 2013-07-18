cccfcc32c5a80c9bd451cdde7783de73ef55d447
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-07-18 18:06:46 +0200

    Removed namespace registration

--- system/modules/isotope/config/autoload.php
+++ system/modules/isotope/config/autoload.php
@@ -14,8 +14,6 @@
 /**
  * Register PSR-0 namespace
  */
-NamespaceClassLoader::add('Isotope/Model/Payment', 'system/modules/isotope/library');
-NamespaceClassLoader::add('Isotope/Model/Shipping', 'system/modules/isotope/library');
 NamespaceClassLoader::add('Isotope/Gallery', 'system/modules/isotope/library');
 NamespaceClassLoader::add('Isotope', 'system/modules/isotope/library');
 
