fbc9014a2711e65fc02b27723ffeb0eff42a6557
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-07-18 15:47:12 +0200

    Added abstract TypeAgent model and TypeAgent model collection

---
+++ system/modules/isotope/library/Isotope/Model/Collection/TypeAgent.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Isotope eCommerce for Contao Open Source CMS
+ *
+ * Copyright (C) 2008-2012 Isotope eCommerce Workgroup
+ *
+ * @package    Isotope
+ * @link       http://www.isotopeecommerce.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ */
+
+namespace Isotope\Model\Collection;
+
+
+class TypeAgent extends \Model\Collection
+{
+
+    /**
+	 * Fetch the next result row and create the model
+	 *
+	 * @return boolean True if there was another row
+	 */
+	protected function fetchNext()
+	{
+		if ($this->objResult->next() == false)
+		{
+			return false;
+		}
+
+		$this->arrModels[$this->intIndex + 1] = call_user_func(array($this->strTable, 'buildModelType'), $this->objResult);
+
+		return true;
+	}
+}
