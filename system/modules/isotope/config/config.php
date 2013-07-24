31f04a901bc620cf241569d7479da9307ca8c8c0
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-07-24 10:51:33 +0200

    Register product collection and product collection surcharge types

--- system/modules/isotope/config/config.php
+++ system/modules/isotope/config/config.php
@@ -209,6 +209,19 @@ $GLOBALS['BE_FFL']['productGroupSelector']   = 'Isotope\Widget\ProductGroupSelec
  */
 \Isotope\Model\Shipping::registerModelType('flat', 'Isotope\Model\Shipping\Flat');
 
+/**
+ * Product collections
+ */
+\Isotope\Model\ProductCollection::registerModelType('cart', 'Isotope\Model\ProductCollection\Cart');
+\Isotope\Model\ProductCollection::registerModelType('order', 'Isotope\Model\ProductCollection\Order');
+
+/**
+ * Product collection surcharge
+ */
+\Isotope\Model\ProductCollectionSurcharge::registerModelType('payment', 'Isotope\Model\ProductCollectionSurcharge\Payment');
+\Isotope\Model\ProductCollectionSurcharge::registerModelType('shipping', 'Isotope\Model\ProductCollectionSurcharge\Shipping');
+\Isotope\Model\ProductCollectionSurcharge::registerModelType('tax', 'Isotope\Model\ProductCollectionSurcharge\Tax');
+
 
 /**
  * Models
