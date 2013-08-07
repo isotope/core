51dd705f2162d537c57c888b9b9b1978916bc040
Jan Reuteler <jan.reuteler@terminal42.ch>
2013-08-07 11:43:14 +0200

    Created partial DCA with sql definitions (without keys)

---
+++ system/modules/isotope/dca/tl_iso_price_tiers.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Isotope eCommerce for Contao Open Source CMS
+ *
+ * Copyright (C) 2009-2012 Isotope eCommerce Workgroup
+ *
+ * @package    Isotope
+ * @link       http://www.isotopeecommerce.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @author     Jan Reuteler <jan.reuteler@terminal42.ch>
+ */
+
+
+/**
+ * Table tl_iso_price_tiers
+ */
+$GLOBALS['TL_DCA']['tl_iso_price_tiers'] = array
+(
+
+    // Config
+    'config' => array
+    (
+        'dataContainer'             => 'Table',
+        //'ptable'                    => '',
+    ),
+
+    // List
+    'list' => array
+    (
+    ),
+
+    // Palettes
+    'palettes' => array
+    (
+    ),
+
+    // Fields
+    'fields' => array
+    (
+
+        'id' => array
+        (
+            'sql'                 =>  "int(10) unsigned NOT NULL auto_increment",
+        ),
+
+        'pid' => array
+        (
+            'sql'                 =>  "int(10) unsigned NOT NULL default '0'",
+        ),
+
+        'tstamp' => array
+        (
+            'sql'                 =>  "int(10) unsigned NOT NULL default '0'",
+        ),
+
+        'min' => array
+        (
+            'sql'                 =>  "int(10) unsigned NOT NULL default '0'",
+        ),
+
+        'price' => array
+        (
+            'sql'                 =>  "decimal(12,2) NOT NULL default '0.00'",
+        ),
+
+    )
+);
