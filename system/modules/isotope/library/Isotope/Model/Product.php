066e4f9174762a486edcacbab7076ec6332bd517
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-08-18 20:45:20 +0200

    Moved product SQL statement to abstract product class

--- system/modules/isotope/library/Isotope/Model/Product.php
+++ system/modules/isotope/library/Isotope/Model/Product.php
@@ -42,4 +42,55 @@ abstract class Product extends TypeAgent
      */
     protected static $arrModelTypes = array();
 
+
+    /**
+     * Return select statement to load product data including multilingual fields
+     * @param array an array of columns
+     * @return string
+     */
+    public static function getSelectStatement($arrColumns=false)
+    {
+        static $strSelect = '';
+
+        if ($strSelect == '' || $arrColumns !== false)
+        {
+            $arrSelect = ($arrColumns !== false) ? $arrColumns : array('p1.*');
+            $arrSelect[] = "'".$GLOBALS['TL_LANGUAGE']."' AS language";
+
+            foreach ($GLOBALS['ISO_CONFIG']['multilingual'] as $attribute)
+            {
+                if ($arrColumns !== false && !in_array('p1.'.$attribute, $arrColumns))
+                    continue;
+
+                $arrSelect[] = "IFNULL(p2.$attribute, p1.$attribute) AS {$attribute}";
+            }
+
+            foreach ($GLOBALS['ISO_CONFIG']['fetch_fallback'] as $attribute)
+            {
+                if ($arrColumns !== false && !in_array('p1.'.$attribute, $arrColumns))
+                    continue;
+
+                $arrSelect[] = "p1.$attribute AS {$attribute}_fallback";
+            }
+
+            $strQuery = "
+SELECT
+    " . implode(', ', $arrSelect) . ",
+    t.class AS product_class,
+    c.sorting
+FROM tl_iso_products p1
+INNER JOIN tl_iso_producttypes t ON t.id=p1.type
+LEFT OUTER JOIN tl_iso_products p2 ON p1.id=p2.pid AND p2.language='" . $GLOBALS['TL_LANGUAGE'] . "'
+LEFT OUTER JOIN tl_iso_product_categories c ON p1.id=c.pid";
+
+            if ($arrColumns !== false)
+            {
+                return $strQuery;
+            }
+
+            $strSelect = $strQuery;
+        }
+
+        return $strSelect;
+    }
 }
