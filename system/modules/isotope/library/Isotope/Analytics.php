e9c0578e6eb6b57ba03cd3351cf12fca104f21d8
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-08-23 19:16:12 +0200

    CS

--- system/modules/isotope/library/Isotope/Analytics.php
+++ system/modules/isotope/library/Isotope/Analytics.php
@@ -16,108 +16,107 @@ namespace Isotope;
 class Analytics extends Frontend
 {
 
-	/**
-	 * Process checkout
-	 */
-	public function trackOrder($objOrder, $arrItemIds, $arrData)
-	{
-		$objConfig = new IsotopeConfig();
+    /**
+     * Process checkout
+     */
+    public function trackOrder($objOrder, $arrItemIds, $arrData)
+    {
+        $objConfig = new IsotopeConfig();
 
-		if ($objConfig->findBy('id', $objOrder->config_id))
-		{
-			if ($objConfig->ga_enable)
-			{
-				$this->trackGATransaction($objConfig, $objOrder);
-			}
-		}
+        if ($objConfig->findBy('id', $objOrder->config_id)) {
 
-		return true;
-	}
+            if ($objConfig->ga_enable) {
+                $this->trackGATransaction($objConfig, $objOrder);
+            }
+        }
+
+        return true;
+    }
 
 
-	/**
-	 * Actually execute the GoogleAnalytics tracking
-	 * @param Database_Result
-	 * @param IsotopeProductCollection
-	 */
-	protected function trackGATransaction($objConfig, $objOrder)
-	{
-		// Initilize GA Tracker
-		$tracker = new \UnitedPrototype\GoogleAnalytics\Tracker($objConfig->ga_account, $this->Environment->base);
+    /**
+     * Actually execute the GoogleAnalytics tracking
+     * @param Database_Result
+     * @param IsotopeProductCollection
+     */
+    protected function trackGATransaction($objConfig, $objOrder)
+    {
+        // Initilize GA Tracker
+        $tracker = new \UnitedPrototype\GoogleAnalytics\Tracker($objConfig->ga_account, $this->Environment->base);
 
-		// Assemble Visitor information
-		// (could also get unserialized from database)
-		$visitor = new \UnitedPrototype\GoogleAnalytics\Visitor();
-		$visitor->setIpAddress($this->Environment->ip);
-		$visitor->setUserAgent($this->Environment->httpUserAgent);
+        // Assemble Visitor information
+        // (could also get unserialized from database)
+        $visitor = new \UnitedPrototype\GoogleAnalytics\Visitor();
+        $visitor->setIpAddress($this->Environment->ip);
+        $visitor->setUserAgent($this->Environment->httpUserAgent);
 
-		$transaction = new \UnitedPrototype\GoogleAnalytics\Transaction();
+        $transaction = new \UnitedPrototype\GoogleAnalytics\Transaction();
 
-		$transaction->setOrderId($objOrder->order_id);
-		$transaction->setAffiliation($objConfig->name);
-		$transaction->setTotal($objOrder->grandTotal);
-		$transaction->setTax($objOrder->taxTotal);
-		$transaction->setShipping($objOrder->shippingTotal);
-		$transaction->setCity($objOrder->billing_address['city']);
+        $transaction->setOrderId($objOrder->order_id);
+        $transaction->setAffiliation($objConfig->name);
+        $transaction->setTotal($objOrder->grandTotal);
+        $transaction->setTax($objOrder->taxTotal);
+        $transaction->setShipping($objOrder->shippingTotal);
+        $transaction->setCity($objOrder->billing_address['city']);
 
-		if($objOrder->billing_address['subdivision'])
-		{
-			$arrSub = explode("-",$objOrder->billing_address['subdivision']);
-			$transaction->setRegion($arrSub[1]);
-		}
+        if ($objOrder->billing_address['subdivision']) {
+            $arrSub = explode("-",$objOrder->billing_address['subdivision']);
+            $transaction->setRegion($arrSub[1]);
+        }
 
-		$transaction->setCountry($objOrder->billing_address['country']);
+        $transaction->setCountry($objOrder->billing_address['country']);
 
-		$arrProducts = $objOrder->getProducts();
+        $arrProducts = $objOrder->getProducts();
 
 
 
-		foreach($arrProducts as $i=>$objProduct)
-		{
-			$item = new \UnitedPrototype\GoogleAnalytics\Item();
+        foreach ($arrProducts as $i=>$objProduct)
+        {
+            $item = new \UnitedPrototype\GoogleAnalytics\Item();
 
-			$arrOptions = array();
-			$arrOptionValues = array();
+            $arrOptions = array();
+            $arrOptionValues = array();
 
-			if($objProduct->sku)
-				$item->setSku($objProduct->sku);
+            if ($objProduct->sku) {
+                $item->setSku($objProduct->sku);
+            }
 
-			$item->setName($objProduct->name);
-			$item->setPrice($objProduct->price);
-			$item->setQuantity($objProduct->quantity_requested);
+            $item->setName($objProduct->name);
+            $item->setPrice($objProduct->price);
+            $item->setQuantity($objProduct->quantity_requested);
 
-			//Do we also potentially have options?
-			$arrOptions = $objProduct->getOptions(true);
+            //Do we also potentially have options?
+            $arrOptions = $objProduct->getOptions(true);
 
-			foreach ($arrOptions as $field => $value)
-			{
-				if ($value == '')
-					continue;
+            foreach ($arrOptions as $field => $value)
+            {
+                if ($value == '')
+                    continue;
 
-				$arrOptionValues[] = $this->Isotope->formatValue('tl_iso_products', $field, $value);
+                $arrOptionValues[] = $this->Isotope->formatValue('tl_iso_products', $field, $value);
 
-			}
+            }
 
-			if(count($arrOptionValues))
-				$item->setVariation(implode(' ',$arrOptionValues));
+            if(count($arrOptionValues))
+                $item->setVariation(implode(' ',$arrOptionValues));
 
-			$transaction->addItem($item);
-		}
+            $transaction->addItem($item);
+        }
 
-		// Track logged-in member as custom variable
-		if ($objConfig->ga_member != '' && FE_USER_LOGGED_IN)
-		{
-			$this->import('FrontendUser', 'User');
+        // Track logged-in member as custom variable
+        if ($objConfig->ga_member != '' && FE_USER_LOGGED_IN)
+        {
+            $this->import('FrontendUser', 'User');
 
-			$customVar = new \UnitedPrototype\GoogleAnalytics\CustomVariable(1, 'Member', $this->parseSimpleTokens($objConfig->ga_member, $this->User->getData()), \UnitedPrototype\GoogleAnalytics\CustomVariable::SCOPE_VISITOR);
+            $customVar = new \UnitedPrototype\GoogleAnalytics\CustomVariable(1, 'Member', $this->parseSimpleTokens($objConfig->ga_member, $this->User->getData()), \UnitedPrototype\GoogleAnalytics\CustomVariable::SCOPE_VISITOR);
 
-			$tracker->addCustomVariable($customVar);
-		}
+            $tracker->addCustomVariable($customVar);
+        }
 
-		// Assemble Session information
-		// (could also get unserialized from PHP session)
-		$session = new \UnitedPrototype\GoogleAnalytics\Session();
+        // Assemble Session information
+        // (could also get unserialized from PHP session)
+        $session = new \UnitedPrototype\GoogleAnalytics\Session();
 
-		$tracker->trackTransaction($transaction, $session, $visitor);
-	}
+        $tracker->trackTransaction($transaction, $session, $visitor);
+    }
 }
