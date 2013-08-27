5fd27009eeb7711bec359f1120954ae97309442a
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-08-27 15:27:53 +0200

    Add model relation to tl_iso_product_collection.member

--- system/modules/isotope/dca/tl_iso_product_collection.php
+++ system/modules/isotope/dca/tl_iso_product_collection.php
@@ -177,7 +177,9 @@ $GLOBALS['TL_DCA']['tl_iso_product_collection'] = array
         ),
         'member'  =>  array
         (
+            'foreignKey'            => 'tl_member.id',
             'sql'                   => "int(10) unsigned NOT NULL default '0'",
+            'relation'              => array('type'=>'hasOne', 'load'=>'lazy'),
         ),
         'locked' => array
         (
