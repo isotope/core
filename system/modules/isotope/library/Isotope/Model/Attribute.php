fb8e597ff805589c4b04de85e59566136125de60
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-08-30 20:56:09 +0200

    Set type when loading from DCA

--- system/modules/isotope/library/Isotope/Model/Attribute.php
+++ system/modules/isotope/library/Isotope/Model/Attribute.php
@@ -102,6 +102,7 @@ abstract class Attribute extends TypeAgent
         }
 
         $this->field_name = $strName;
+        $this->type = $this->getClassForModelType(get_called_class());
         $this->name = is_array($arrField['label']) ? $arrField['label'][0] : ($arrField['label'] ?: $strName);
         $this->description = is_array($arrField['label']) ? $arrField['label'][1] : '';
         $this->be_filter = $arrField['filter'] ? '1' : '';
