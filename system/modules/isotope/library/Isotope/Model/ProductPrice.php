e559357b2135194d396444bc98efe41d5e293b06
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-08-30 23:13:54 +0200

    Currently can't pass product because of endless loop

--- system/modules/isotope/library/Isotope/Model/ProductPrice.php
+++ system/modules/isotope/library/Isotope/Model/ProductPrice.php
@@ -72,7 +72,8 @@ class ProductPrice extends \Model implements IsotopePrice
      */
     public function getAmount($intQuantity=1)
     {
-        return Isotope::calculatePrice($this->getValueForTier($intQuantity), $this->getRelated('pid'), 'price', $this->tax_class);
+        // @todo should pass product object as second parameter
+        return Isotope::calculatePrice($this->getValueForTier($intQuantity), null, 'price', $this->tax_class);
     }
 
     /**
@@ -82,7 +83,8 @@ class ProductPrice extends \Model implements IsotopePrice
      */
     public function getOriginalAmount($intQuantity=1)
     {
-        return Isotope::calculatePrice($this->getValueForTier($intQuantity), $this->getRelated('pid'), 'original_price', $this->tax_class);
+        // @todo should pass product object as second parameter
+        return Isotope::calculatePrice($this->getValueForTier($intQuantity), null, 'original_price', $this->tax_class);
     }
 
     /**
@@ -98,7 +100,8 @@ class ProductPrice extends \Model implements IsotopePrice
             $objTaxClass->calculateNetPrice($fltAmount);
         }
 
-        return Isotope::calculatePrice($fltAmount, $this->getRelated('pid'), 'net_price');
+        // @todo should pass product object as second parameter
+        return Isotope::calculatePrice($fltAmount, null, 'net_price');
     }
 
     /**
@@ -114,7 +117,8 @@ class ProductPrice extends \Model implements IsotopePrice
             $objTaxClass->calculateGrossPrice($fltAmount);
         }
 
-        return Isotope::calculatePrice($fltAmount, $this->getRelated('pid'), 'gross_price');
+        // @todo should pass product object as second parameter
+        return Isotope::calculatePrice($fltAmount, null, 'gross_price');
     }
 
     /**
@@ -127,7 +131,8 @@ class ProductPrice extends \Model implements IsotopePrice
             return $this->getAmount();
         }
 
-        return Isotope::calculatePrice(min($this->arrTiers), $this->getRelated('pid'), 'price', $this->tax_class);
+        // @todo should pass product object as second parameter
+        return Isotope::calculatePrice(min($this->arrTiers), null, 'price', $this->tax_class);
     }
 
     /**
