0a53cc3456310c336b42e8896600e547ec24e6b5
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-08-30 15:53:01 +0200

    Product type now compiles the list of available attributes

--- system/modules/isotope/library/Isotope/Model/ProductType.php
+++ system/modules/isotope/library/Isotope/Model/ProductType.php
@@ -29,11 +29,79 @@ class ProductType extends \Model
     protected static $strTable = 'tl_iso_producttypes';
 
     /**
+     * Product attributes
+     * @var array
+     */
+    protected $arrAttributes;
+
+    /**
+     * Product variant attributes
+     * @var array
+     */
+    protected $arrVariantAttributes;
+
+
+    /**
      * Returns true if variants are enabled in the product type, otherwise returns false
      * @return  bool
      */
     public function hasVariants()
     {
         return (bool) $this->variants;
+    }
+
+    /**
+     * Get enabled attributes by sorting
+     * @return  array
+     */
+    public function getAttributes()
+    {
+        if (null === $this->arrAttributes) {
+            $this->arrAttributes = $this->getEnabledAttributesByPosition($this->attributes);
+        }
+
+        return $this->arrAttributes;
+    }
+
+    /**
+     * Get enabled variant attributes by sorting
+     * @return  array
+     */
+    public function getVariantAttributes()
+    {
+        if (null === $this->arrVariantAttributes) {
+
+            if (!$this->hasVariants()) {
+                $this->arrVariantAttributes = array();
+            } else {
+                $this->arrVariantAttributes = $this->getEnabledAttributesByPosition($this->variant_attributes);
+            }
+        }
+
+        return $this->arrVariantAttributes;
+    }
+
+    /**
+     * Sort the attributes based on their position (from wizard) and return their names only
+     * @param   mixed
+     * @return  array
+     */
+    protected function getEnabledAttributesByPosition($varValue)
+    {
+        $arrAttributes = deserialize($varValue, true);
+
+        array_filter($arrAttributes, function($a) {
+            if ($a['enabled']) {
+                return true;
+            }
+
+            return false;
+        });
+
+        uasort($arrAttributes, function ($a, $b) {
+            return $a["position"] > $b["position"];
+        });
+
+        return array_keys($arrAttributes);
     }
 }
