ef258037649d3edc53f6777b58038ba9b811c3ee
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-09-12 20:56:24 +0200

    Added Label model

--- system/modules/isotope/config/config.php
+++ system/modules/isotope/config/config.php
@@ -265,6 +265,7 @@ $GLOBALS['TL_MODELS']['tl_iso_config']                          = 'Isotope\Model
 $GLOBALS['TL_MODELS']['tl_iso_addresses']                       = 'Isotope\Model\Address';
 $GLOBALS['TL_MODELS']['tl_iso_baseprice']                       = 'Isotope\Model\BasePrice';
 $GLOBALS['TL_MODELS']['tl_iso_downloads']                       = 'Isotope\Model\Download';
+$GLOBALS['TL_MODELS']['tl_iso_labels']                          = 'Isotope\Model\Label';
 $GLOBALS['TL_MODELS']['tl_iso_orderstatus']                     = 'Isotope\Model\OrderStatus';
 $GLOBALS['TL_MODELS']['tl_iso_product_collection']              = 'Isotope\Model\ProductCollection';
 $GLOBALS['TL_MODELS']['tl_iso_product_collection_item']         = 'Isotope\Model\ProductCollectionItem';
