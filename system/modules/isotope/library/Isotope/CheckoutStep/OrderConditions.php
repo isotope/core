26e3dd44f5fc96f002f50fb1171eb06bd239d11a
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-09-22 00:33:54 +0200

    Updated OrderConditions to return form field data as email tokens

--- system/modules/isotope/library/Isotope/CheckoutStep/OrderConditions.php
+++ system/modules/isotope/library/Isotope/CheckoutStep/OrderConditions.php
@@ -21,6 +21,12 @@ abstract class OrderConditions extends CheckoutStep
 {
 
     /**
+     * Haste form
+     * @var object
+     */
+    protected $objForm;
+
+    /**
      * Returns true if order conditions are defined
      * @return  bool
      */
@@ -35,36 +41,28 @@ abstract class OrderConditions extends CheckoutStep
      */
     public function generate()
     {
-        $objForm = new Form($this->objModule->getFormId(), 'POST', function($haste) {
+        $this->objForm = new Form($this->objModule->getFormId(), 'POST', function($haste) {
             return \Input::post('FORM_SUBMIT') === $haste->getFormId();
         }, (boolean) $this->objModule->tableless);
 
         // don't catch the exception here because we want it to be shown to the user
-        $objForm->addFieldsFromFormGenerator($this->objModule->iso_order_conditions);
+        $this->objForm->addFieldsFromFormGenerator($this->objModule->iso_order_conditions);
 
         // Manually create widgets because we need to know if there are uploadable widgets
-        $objForm->createWidgets();
+        $this->objForm->createWidgets();
 
         // change enctype if there are uploads
-        if ($objForm->hasUploads()) {
+        if ($this->objForm->hasUploads()) {
             $this->objModule->Template->enctype = 'multipart/form-data';
         }
 
-        if ($objForm->isSubmitted() && $objForm->validate()) {
-            foreach ($objForm->fetchAll() as $strName => $varValue) {
-                if ($objForm->getWidget($strName) instanceof \uploadable) {
-                    $arrFile = $_SESSION['FILES'][$strName];
-                    $varValue = str_replace(TL_ROOT . '/', '', dirname($arrFile['tmp_name'])) . '/' . rawurlencode($arrFile['name']);
-                }
-
-                $this->objModule->arrOrderData['form_' . $strName] = $varValue;
-            }
-        } else {
+        if (!$this->objForm->isSubmitted() || !$this->objForm->validate()) {
             $this->blnError = true;
         }
 
         $objTemplate = new \Isotope\Template('iso_checkout_order_conditions');
-        $objForm->addToTemplate($objTemplate);
+        $this->objForm->addToTemplate($objTemplate);
+
         return $objTemplate->parse();
     }
 
@@ -74,25 +72,6 @@ abstract class OrderConditions extends CheckoutStep
      */
     public function review()
     {
-        if (!$this->hasError())
-        {
-            if (is_array($_SESSION['FORM_DATA']))
-            {
-                foreach( $_SESSION['FORM_DATA'] as $name => $value )
-                {
-                    $this->objModule->arrOrderData['form_' . $name] = $value;
-                }
-            }
-
-            if (is_array($_SESSION['FILES']))
-            {
-                foreach( $_SESSION['FILES'] as $name => $file )
-                {
-                    $this->objModule->arrOrderData['form_' . $name] = str_replace(TL_ROOT . '/', '', dirname($file['tmp_name'])) . '/' . rawurlencode($file['name']);
-                }
-            }
-        }
-
         return '';
     }
 
@@ -104,19 +83,17 @@ abstract class OrderConditions extends CheckoutStep
      */
     public function getEmailTokens(IsotopeProductCollection $objCollection, \Module $objModule)
     {
-        // @todo return form field values
-        /*
-        foreach ($objForm->arrFormData as $name => $value)
-        {
-            $this->objModule->arrOrderData['form_' . $name] = $value;
+        $arrTokens = array();
+
+        foreach ($this->objForm->fetchAll() as $strName => $varValue) {
+            if ($this->objForm->getWidget($strName) instanceof \uploadable) {
+                $arrFile = $_SESSION['FILES'][$strName];
+                $varValue = str_replace(TL_ROOT . '/', '', dirname($arrFile['tmp_name'])) . '/' . rawurlencode($arrFile['name']);
+            }
+
+            $arrTokens['form_' . $strName] = $varValue;
         }
 
-        foreach ($objForm->arrFiles as $name => $file)
-        {
-            $this->objModule->arrOrderData['form_' . $name] = \Environment::get('base') . str_replace(TL_ROOT . '/', '', dirname($file['tmp_name'])) . '/' . rawurlencode($file['name']);
-        }
-        */
-
-        return array();
+        return $arrTokens;
     }
 }
