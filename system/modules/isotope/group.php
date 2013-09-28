9bc340778a8d7363db9f8bf030f194230ec0c9ac
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-09-27 21:44:17 -0400

    Hide group manager if user cannot manage groups (#721)

--- system/modules/isotope/group.php
+++ system/modules/isotope/group.php
@@ -112,10 +112,13 @@ class ProductGroupPicker extends \Backend
         $this->Template->search = $GLOBALS['TL_LANG']['MSC']['search'];
         $this->Template->action = ampersand(\Environment::get('request'));
         $this->Template->value = $this->Session->get('product_group_selector_search');
-        $this->Template->manager = $GLOBALS['TL_LANG']['tl_iso_groups']['manager'];
-        $this->Template->managerHref = 'contao/main.php?do=iso_products&amp;table=tl_iso_groups&amp;popup=1';
         $this->Template->addSearch = true;
         $this->Template->breadcrumb = \Isotope\Backend::generateGroupsBreadcrumb($this->Session->get('iso_products_gid'));
+
+        if ($this->User->isAdmin || (is_array($this->User->iso_groupp) && !empty($this->User->iso_groupp))) {
+            $this->Template->manager = $GLOBALS['TL_LANG']['tl_iso_groups']['manager'];
+            $this->Template->managerHref = 'contao/main.php?do=iso_products&amp;table=tl_iso_groups&amp;popup=1';
+        }
 
         $GLOBALS['TL_CONFIG']['debugMode'] = false;
         $this->Template->output();
