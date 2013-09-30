ea052b58547f474ad701a4b5357fb16d57c2cecb
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-09-30 11:59:09 -0400

    Temporarily added german language files
    These are from Isotope 1.4 and should be completely rewritten.
    For beta1 we include them to have at least some german translations.

---
+++ system/modules/isotope/languages/de/tl_content.php
@@ -0,0 +1,41 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Isotope eCommerce Workgroup 2009-2012
+ * @author     Andreas Burg <ab@andreasburg.de>
+ * @author     Nikolas Runde <info@nrmedia.de>
+ * @author     Patrick Grob <grob@a-sign.ch>
+ * @author     Frank Berger <berger@mediastuff.de>
+ * @author     Oliver Hoff <oliver@hoff.com>
+ * @author     Stefan Preiss <stefan@preiss-at-work.de>
+ * @author     Nina Gerling <gerling@ena-webstudio.de>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+$GLOBALS['TL_LANG']['tl_content']['iso_reader_jumpTo'][0] = 'Produktleser-Weiterleitungsseite';
+$GLOBALS['TL_LANG']['tl_content']['iso_reader_jumpTo'][1] = 'Diese Einstellung legt fest zu welcher Seite der Nutzer geleitet wird, wenn er bei einem Produkt weitere Informationen anklickt.';
+$GLOBALS['TL_LANG']['tl_content']['iso_list_layout'][0] = 'Produktlisten-Template';
+$GLOBALS['TL_LANG']['tl_content']['iso_list_layout'][1] = 'Bitte wählen Sie ein Listen-Layout. Sie können neue angepasste Listen-Layouts in den Ordner <em>templates</em> hinzufügen. Listen-Templates beginnen mit <em>iso_list_</em> und benötigen die Dateiendung <em>.html5, .xhtml</em> oder <em>.tpl</em>.';
+$GLOBALS['TL_LANG']['tl_content']['iso_attribute_set'][0] = 'Artikelmerkmal-Sammlung';
+$GLOBALS['TL_LANG']['tl_content']['iso_attribute_set'][1] = 'Wählen Sie eine Artikelmerkmal-Sammlung um einen Filter für diese Seite zu wählen.';
+
---
+++ system/modules/isotope/languages/de/tl_iso_addresses.php
@@ -0,0 +1,92 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Isotope eCommerce Workgroup 2009-2012
+ * @author     Andreas Burg <ab@andreasburg.de>
+ * @author     Nikolas Runde <info@nrmedia.de>
+ * @author     Patrick Grob <grob@a-sign.ch>
+ * @author     Frank Berger <berger@mediastuff.de>
+ * @author     Oliver Hoff <oliver@hoff.com>
+ * @author     Stefan Preiss <stefan@preiss-at-work.de>
+ * @author     Nina Gerling <gerling@ena-webstudio.de>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+$GLOBALS['TL_LANG']['tl_iso_addresses']['label'][0] = 'Bezeichnung';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['label'][1] = 'Sie können dieser Adresse eine Bezeichnung geben.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['store_id'][0] = 'Shop-ID';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['store_id'][1] = 'Nutzen Sie verschiedene Shop-IDs um mehrere Shop-Konfigurationen zu gruppieren. Der Warenkorb und die Adressen des Nutzers werden übergreifend zwischen Shops mit identischer ID genutzt.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['salutation'][0] = 'Anrede';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['salutation'][1] = 'Bitte geben Sie die Anrede ein (Herr, Frau, Dr., Prof.).';
+
+$GLOBALS['TL_LANG']['tl_iso_addresses']['firstname'][0] = 'Vorname';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['firstname'][1] = 'Bitte geben Sie den Vornamen ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['lastname'][0] = 'Nachname';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['lastname'][1] = 'Bitte geben Sie den Nachnamen ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['company'][0] = 'Firma';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['company'][1] = 'Hier können Sie einen Firmennamen eingeben.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['vat_no'][0] = 'Ust-ID Nr.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['vat_no'][1] = 'Bitte geben Sie ihre Umsatzsteuer-ID Nummer ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_1'][0] = 'Straße';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_1'][1] = 'Bitte geben Sie den Straßennamen und die Hausnummer ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_2'][0] = 'Straße 2';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_2'][1] = 'Geben Sie eine zweite Zeile für die Straße ein falls nötig.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_3'][0] = 'Straße 3';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['street_3'][1] = 'Geben Sie eine dritte Zeile für die Straße ein falls nötig.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['postal'][0] = 'Postleitzahl';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['postal'][1] = 'Bitte geben Sie die Postleitzahl ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['city'][0] = 'Ort';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['city'][1] = 'Bitte geben Sie den Namen des Ortes ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['subdivision'][0] = 'Staat';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['subdivision'][1] = 'Bitte geben Sie den Namen des Staates ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['country'][0] = 'Land';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['country'][1] = 'Bitte wählen Sie ein Land.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['phone'][0] = 'Telefonnummer';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['phone'][1] = 'Bitte geben Sie die Telefonnummer ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['email'][0] = 'E-Mail-Adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['email'][1] = 'Bitte geben Sie eine gültige E-Mail-Adresse ein.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['isDefaultBilling'][0] = 'Standard-Rechnungsadresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['isDefaultBilling'][1] = 'Klicken Sie hier wenn dies die Standard-Rechnungsadresse ist.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['isDefaultShipping'][0] = 'Standard-Versandadresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['isDefaultShipping'][1] = 'Klicken Sie hier wenn dies die Standard-Versandadresse ist.';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['store_legend'] = 'Shop';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['personal_legend'] = 'Personendaten';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['address_legend'] = 'Adressdaten';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['contact_legend'] = 'Kontaktdaten';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['default_legend'] = 'Standard-Adressen';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['personalData'] = 'Persönliche Angaben';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['addressDetails'] = 'Adressdetails';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['contactDetails'] = 'Kontaktdetails';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['loginDetails'] = 'Standard-Adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['new'][0] = 'Neue Adresse';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['new'][1] = 'Eine neue Adresse anlegen';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['show'][0] = 'Adressdetails';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['show'][1] = 'Details der Adresse ID %s anzeigen';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['edit'][0] = 'Adresse bearbeiten';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['edit'][1] = 'Adresse ID %s bearbeiten';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['copy'][0] = 'Adresse duplizieren';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['copy'][1] = 'Adresse ID %s duplizieren';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['delete'][0] = 'Adresse löschen';
+$GLOBALS['TL_LANG']['tl_iso_addresses']['delete'][1] = 'Adresse ID %s löschen';
+
