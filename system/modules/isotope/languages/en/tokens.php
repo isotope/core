38477339329e304150ffbf0696fe7cc80d214772
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-10-08 09:27:09 +0200

    Fixed token descriptions

--- system/modules/isotope/languages/en/tokens.php
+++ system/modules/isotope/languages/en/tokens.php
@@ -17,24 +17,24 @@
 /**
  * Order confirmation
  */
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['billing_*']             = 'All the billing address model fields';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['billing_address']       = 'Billing address as HTML';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['billing_address_text']  = 'Billing address as text';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['shipping_*']            = 'All the shipping address model fields';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['shipping_address']      = 'Shipping address as HTML';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['shipping_address_text'] = 'Shipping address as text';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['form_*']                = 'All the order condition form fields';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['items']                 = 'Number of items in order';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['products']              = 'Number of single products in order';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['subTotal']              = 'Subtotal';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['grandTotal']            = 'Grand total';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['cart_text']             = 'Order/Cart as text';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['cart_html']             = 'Order/Cart as HTML';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['payment_id']            = 'Payment method ID';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['payment_label']         = 'Payment method label';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['payment_note']          = 'Payment method note';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['payment_note_text']     = 'Payment method note without HTML tags';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['shipping_id']           = 'Shipping method ID';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['shipping_label']        = 'Shipping method label';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['shipping_note']         = 'Shipping method note';
-$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_confirmation']['shipping_note_text']    = 'Shipping method note without HTML tags';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['billing_*']             = 'All the billing address model fields';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['billing_address']       = 'Billing address as HTML';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['billing_address_text']  = 'Billing address as text';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['shipping_*']            = 'All the shipping address model fields';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['shipping_address']      = 'Shipping address as HTML';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['shipping_address_text'] = 'Shipping address as text';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['form_*']                = 'All the order condition form fields';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['items']                 = 'Number of items in order';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['products']              = 'Number of single products in order';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['subTotal']              = 'Subtotal';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['grandTotal']            = 'Grand total';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['cart_text']             = 'Order/Cart as text';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['cart_html']             = 'Order/Cart as HTML';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['payment_id']            = 'Payment method ID';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['payment_label']         = 'Payment method label';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['payment_note']          = 'Payment method note';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['payment_note_text']     = 'Payment method note without HTML tags';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['shipping_id']           = 'Shipping method ID';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['shipping_label']        = 'Shipping method label';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['shipping_note']         = 'Shipping method note';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['iso_order_status_change']['shipping_note_text']    = 'Shipping method note without HTML tags';
