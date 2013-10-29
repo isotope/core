ef89dd2fcca68e83c8aa2bb68cd41d9f2614d637
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-10-29 18:14:23 +0100

    Import new translation for Czech

---
+++ system/modules/isotope/languages/cs/tl_iso_attributes.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Isotope eCommerce for Contao Open Source CMS
+ * 
+ * Copyright (C) 2009-2013 Isotope eCommerce Workgroup
+ * 
+ * Core translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ * 
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/i/isotope/language/cs/
+ * 
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+$GLOBALS['TL_LANG']['tl_iso_attributes']['name'][0] = 'Jméno';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['copy'][0] = 'Kopírovat atribut';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['copy'][1] = 'Kopírovat ID atributu %s.';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['delete'][0] = 'Smazat atribut';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['show'][0] = 'Detaily atributu';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['show'][1] = 'Ukaž detaily atributu s ID %s.';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['attribute_legend'] = 'jméno & typ atributu';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['description_legend'] = 'Popis';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['options_legend'] = 'Možnosti';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['config_legend'] = 'Nastavení atributu';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['search_filters_legend'] = 'Vyhledávání  & Filtrace nastavení';
+$GLOBALS['TL_LANG']['tl_iso_attributes']['store_legend'] = 'Skladovací složka';
