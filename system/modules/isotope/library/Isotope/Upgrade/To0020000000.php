4e0a789d9f60b9234ead64b35777d47c3d90e493
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-10-30 23:05:15 +0100

    Prevent data loss when old Isotope is installed

--- system/modules/isotope/library/Isotope/Upgrade/To0020000000.php
+++ system/modules/isotope/library/Isotope/Upgrade/To0020000000.php
@@ -18,5 +18,40 @@ class To0020000000 extends \System
 
     public function run($blnInstalled)
     {
+        $this->checkForOld();
+    }
+
+
+    protected function checkForOld()
+    {
+        foreach (array(
+            'tl_store', // tl_iso_config in 0.2
+            'tl_iso_orders',
+            'tl_iso_order_items',
+            'tl_iso_cart',
+            'tl_iso_cart_items',
+            'tl_iso_mail',
+            'tl_iso_mail_content'
+        ) as $strOldTable) {
+            if (\Database::getInstance()->tableExists($strOldTable)) {
+                $this->warnForOld();
+            }
+        }
+
+        if (in_array('isotope_multilingual', \Config::getInstance()->getActiveModules())) {
+            $this->warnForOld();
+        }
+    }
+
+
+    /**
+     * Output warning about old Isotope version that can't be updated
+     */
+    protected function warnForOld()
+    {
+        $objTemplate = new \BackendTemplate('be_iso_old');
+
+        $objTemplate->output();
+        exit;
     }
 }
