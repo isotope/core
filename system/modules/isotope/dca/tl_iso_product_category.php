81e6e0ed28f8c7592b7e8e240f50141c2afb339b
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-11-01 17:51:10 +0100

    Re-added DC_TablePageId

--- system/modules/isotope/dca/tl_iso_product_category.php
+++ system/modules/isotope/dca/tl_iso_product_category.php
@@ -23,8 +23,14 @@ $GLOBALS['TL_DCA']['tl_iso_product_category'] = array
     // Config
     'config' => array
     (
-        'dataContainer'             => 'Table',
-        'ptable'                    => 'tl_iso_product',
+        'dataContainer'             => 'TablePageId',
+        'ptable'                    => 'tl_page',
+        'closed'                    => true,
+        'notEditable'               => true,
+        'oncut_callback' => array
+        (
+            array('Isotope\Backend', 'truncateProductCache'),
+        ),
         'sql' => array
         (
             'keys' => array
@@ -35,7 +41,45 @@ $GLOBALS['TL_DCA']['tl_iso_product_category'] = array
         ),
     ),
 
-    // Fields
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                  => 4,
+            'fields'                => array('sorting'),
+            'panelLayout'           => 'limit',
+            'headerFields'          => array('title', 'type'),
+            'child_record_callback' => array('Isotope\tl_iso_product_category', 'listRows')
+        ),
+        'global_operations' => array
+        (
+            'view' => array
+            (
+                'label'             => &$GLOBALS['TL_LANG']['MSC']['fePreview'],
+                'class'             => 'header_preview',
+                'button_callback'   => array('Isotope\tl_iso_product_category', 'getPageViewButton'),
+            ),
+            'all' => array
+            (
+                'label'             => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'              => 'act=select',
+                'class'             => 'header_edit_all',
+                'attributes'        => 'onclick="Backend.getScrollOffset();"'
+            ),
+        ),
+        'operations' => array
+        (
+            'cut' => array
+            (
+                'label'             => &$GLOBALS['TL_LANG']['tl_iso_product_category']['cut'],
+                'href'              => 'act=paste&amp;mode=cut',
+                'icon'              => 'cut.gif',
+                'attributes'        => 'onclick="Backend.getScrollOffset();"'
+            ),
+        )
+    ),
+
     'fields' => array
     (
         'id' => array
