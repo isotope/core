ed2ceac90a5659a3f2c3dd6c2053ee0d40525ec8
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-12-21 18:43:16 +0100

    CS

--- system/modules/isotope/library/Isotope/Model/Address.php
+++ system/modules/isotope/library/Isotope/Model/Address.php
@@ -66,10 +66,10 @@ class Address extends \Model
     public function generateHtml($arrFields = null)
     {
         // We need a country to format the address, use default country if none is available
-        $strCountry = $this->country != '' ? $this->country : Isotope::getConfig()->country;
+        $strCountry = $this->country ?: Isotope::getConfig()->country;
 
         // Use generic format if no country specific format is available
-        $strFormat = $GLOBALS['ISO_ADR'][$strCountry] != '' ? $GLOBALS['ISO_ADR'][$strCountry] : $GLOBALS['ISO_ADR']['generic'];
+        $strFormat = $GLOBALS['ISO_ADR'][$strCountry] ?: $GLOBALS['ISO_ADR']['generic'];
 
         $arrTokens  = $this->getTokens($arrFields);
         $strAddress = \String::parseSimpleTokens($strFormat, $arrTokens);
