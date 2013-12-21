c4c7ad71c7c57d742eee5bcc69397abda97f1bd5
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-12-21 19:13:37 +0100

    Use table name from model

--- system/modules/isotope/library/Isotope/Backend/Address/Callback.php
+++ system/modules/isotope/library/Isotope/Backend/Address/Callback.php
@@ -54,7 +54,7 @@ class Callback extends \Backend
     {
         if ($varValue == '1' && $dc->activeRecord->{$dc->field} != $varValue) {
             \Database::getInstance()->prepare("
-                UPDATE tl_iso_address SET {$dc->field}='' WHERE pid=? AND ptable=? AND store_id=?
+                UPDATE " . \MemberModel::getTable() . " SET {$dc->field}='' WHERE pid=? AND ptable=? AND store_id=?
             ")->execute($dc->activeRecord->pid, $dc->activeRecord->ptable, $dc->activeRecord->store_id);
         }
 
