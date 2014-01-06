bc6e0a50998756193c0b428c4f8f97f22fd840e1
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-01-06 20:24:37 +0100

    Fixed version constant

--- system/modules/isotope/library/Isotope/Isotope.php
+++ system/modules/isotope/library/Isotope/Isotope.php
@@ -36,7 +36,7 @@ class Isotope extends \Controller
     /**
      * Isotope version
      */
-    const VERSION = '2.1.rc1';
+    const VERSION = '2.1.dev';
 
     /**
      * True if the system has been initialized
