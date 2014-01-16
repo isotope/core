6988e6d323104ff0e12cba1cfe5b4e22a181e1a1
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-01-16 11:25:47 +0100

    Prevent duplicate data conversion in the 2.0.1 migration

--- system/modules/isotope/library/Isotope/Upgrade/To0020000019.php
+++ system/modules/isotope/library/Isotope/Upgrade/To0020000019.php
@@ -18,13 +18,24 @@ class To0020000019 extends \System
 
     public function run($blnInstalled)
     {
-        if ($blnInstalled && \Database::getInstance()->tableExists('tl_iso_rules')) {
-            \Database::getInstance()->query("UPDATE tl_iso_rules SET configCondition='0' WHERE configCondition='1'");
-            \Database::getInstance()->query("UPDATE tl_iso_rules SET configCondition='1' WHERE configCondition=''");
-            \Database::getInstance()->query("UPDATE tl_iso_rules SET memberCondition='0' WHERE memberCondition='1'");
-            \Database::getInstance()->query("UPDATE tl_iso_rules SET memberCondition='1' WHERE memberCondition=''");
-            \Database::getInstance()->query("UPDATE tl_iso_rules SET productCondition='0' WHERE productCondition='1'");
-            \Database::getInstance()->query("UPDATE tl_iso_rules SET productCondition='1' WHERE productCondition=''");
+        if ($blnInstalled && \Database::getInstance()->tableExists('tl_iso_rule')) {
+            foreach (\Database::getInstance()->listFields('tl_iso_rule') as $arrField) {
+
+                if ($arrField['name'] == 'configCondition' && $arrField['type'] == 'char') {
+                    \Database::getInstance()->query("UPDATE tl_iso_rule SET configCondition='0' WHERE configCondition='1'");
+                    \Database::getInstance()->query("UPDATE tl_iso_rule SET configCondition='1' WHERE configCondition=''");
+                }
+
+                if ($arrField['name'] == 'memberCondition' && $arrField['type'] == 'char') {
+                    \Database::getInstance()->query("UPDATE tl_iso_rule SET memberCondition='0' WHERE memberCondition='1'");
+                    \Database::getInstance()->query("UPDATE tl_iso_rule SET memberCondition='1' WHERE memberCondition=''");
+                }
+
+                if ($arrField['name'] == 'productCondition' && $arrField['type'] == 'char') {
+                    \Database::getInstance()->query("UPDATE tl_iso_rule SET productCondition='0' WHERE productCondition='1'");
+                    \Database::getInstance()->query("UPDATE tl_iso_rule SET productCondition='1' WHERE productCondition=''");
+                }
+            }
         }
     }
 }
