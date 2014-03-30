3e2ab64da8778e9cd5a1b9c1589866dc07b2df95
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-03-30 21:58:06 +0200

    Regular users could not filter by product group

--- system/modules/isotope/docs/CHANGELOG-2.0.md
+++ system/modules/isotope/docs/CHANGELOG-2.0.md
@@ -14,6 +14,7 @@ Version 2.0.6 (2014-??-??)
 - Shipping address was unavailable when no shipping method was set (#985)
 - Potential error message when no filter modules were found (#974)
 - in_array error message when editing product as regular user
+- Regular users could not filter by product group
 
 
 Version 2.0.5 (2014-03-24)
--- system/modules/isotope/drivers/DC_ProductData.php
+++ system/modules/isotope/drivers/DC_ProductData.php
@@ -64,10 +64,11 @@ class DC_ProductData extends \DC_Table
                 if (\BackendUser::getInstance()->isAdmin || !is_array(\BackendUser::getInstance()->iso_groups)) {
                     $this->intGroupId = 0;
                 }
-            } elseif (!\BackendUser::getInstance()->isAdmin) {
-                $this->intGroupId = (int) \Database::getInstance()->prepare(
-                    "SELECT id FROM " . \Isotope\Model\Group::getTable() . " WHERE id IN ('" . implode("','", \BackendUser::getInstance()->iso_groups) . "') ORDER BY " . \Database::getInstance()->findInSet('id', \BackendUser::getInstance()->iso_groups)
-                )->limit(1)->execute()->id;
+                elseif (!\BackendUser::getInstance()->isAdmin) {
+                    $this->intGroupId = (int) \Database::getInstance()->prepare(
+                        "SELECT id FROM " . \Isotope\Model\Group::getTable() . " WHERE id IN ('" . implode("','", \BackendUser::getInstance()->iso_groups) . "') ORDER BY " . \Database::getInstance()->findInSet('id', \BackendUser::getInstance()->iso_groups)
+                    )->limit(1)->execute()->id;
+                }
             }
         }
 
