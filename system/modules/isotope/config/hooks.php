26517a17d4f5d1786beda285224d31d08d5fab03
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-03-31 11:52:43 +0200

    CS

--- system/modules/isotope/config/hooks.php
+++ system/modules/isotope/config/hooks.php
@@ -29,13 +29,13 @@ $GLOBALS['ISO_HOOKS']['findSurchargesForCollection'][]  = array('Isotope\Fronten
 
 if (TL_MODE == 'FE') {
     // Only limit countries in FE
-    $GLOBALS['TL_HOOKS']['loadDataContainer'][]        = array('Isotope\Backend\Member\Callback', 'limitCountries');
+    $GLOBALS['TL_HOOKS']['loadDataContainer'][]         = array('Isotope\Backend\Member\Callback', 'limitCountries');
 }
 
 if (TL_MODE == 'BE') {
     // Type agent help is only needed in back end
-    $GLOBALS['TL_HOOKS']['loadDataContainer'][]        = array('Isotope\Backend', 'loadTypeAgentHelp');
+    $GLOBALS['TL_HOOKS']['loadDataContainer'][]         = array('Isotope\Backend', 'loadTypeAgentHelp');
 
     // Adjust the product groups manager
-    $GLOBALS['TL_HOOKS']['parseTemplate'][]            = array('Isotope\Backend', 'adjustGroupsManager');
+    $GLOBALS['TL_HOOKS']['parseTemplate'][]             = array('Isotope\Backend', 'adjustGroupsManager');
 }
