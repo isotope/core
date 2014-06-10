928c6a672efe7b769febe7ea9fa14da9c3491ee6
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-06-10 18:51:37 +0200

    Added new interfaces for attribute options and variant attributes

---
+++ system/modules/isotope/library/Isotope/Interfaces/IsotopeAttributeWithOptions.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Isotope eCommerce for Contao Open Source CMS
+ *
+ * Copyright (C) 2009-2014 terminal42 gmbh & Isotope eCommerce Workgroup
+ *
+ * @package    Isotope
+ * @link       http://isotopeecommerce.org
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+namespace Isotope\Interfaces;
+
+interface IsotopeAttributeWithOptions extends IsotopeAttribute
+{
+
+    /**
+     * Get field options
+     * @return  array
+     */
+    public function getOptions();
+}
