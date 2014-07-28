8e5c3f91d5eb620edac782b1a1bf5734c730ad46
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-07-28 15:03:39 +0200

    Variant options from foreignKey are now allowed

--- system/modules/isotope/library/Isotope/Model/Attribute.php
+++ system/modules/isotope/library/Isotope/Model/Attribute.php
@@ -133,6 +133,7 @@ abstract class Attribute extends TypeAgent
         $this->be_filter   = $arrField['filter'] ? '1' : '';
         $this->be_search   = $arrField['search'] ? '1' : '';
         $this->foreignKey  = $arrField['foreignKey'];
+        $this->optionsSource = '';
     }
 
     /**
@@ -180,23 +181,15 @@ abstract class Attribute extends TypeAgent
             $arrField['eval']['mandatory'] = true;
         }
 
-        // Parse multiline/multilingual foreignKey
-        $this->foreignKey = $this->parseForeignKey($this->foreignKey, $GLOBALS['TL_LANGUAGE']);
-
         // Prepare options
-        if ($this->optionsSource == 'foreignKey' && /* @todo in 3.0: !($this instanceof IsotopeAttributeForVariants) && */!$this->isVariantOption()) {
-            $arrField['foreignKey'] = $this->foreignKey;
+        if ($this->optionsSource == 'foreignKey') {
+            $arrField['foreignKey'] = $this->parseForeignKey($this->foreignKey, $GLOBALS['TL_LANGUAGE']);
             $arrField['eval']['includeBlankOption'] = true;
             unset($arrField['options']);
             unset($arrField['reference']);
 
         else {
-            if ($this->foreignKey) {
-                $arrKey     = explode('.', $this->foreignKey, 2);
-                $arrOptions = \Database::getInstance()->execute("SELECT id AS value, {$arrKey[1]} AS label FROM {$arrKey[0]} ORDER BY label")->fetchAllAssoc();
-            } else {
-                $arrOptions = deserialize($this->options);
-            }
+            $arrOptions = deserialize($this->options);
 
             if (!empty($arrOptions) && is_array($arrOptions)) {
                 $arrField['default'] = array();
