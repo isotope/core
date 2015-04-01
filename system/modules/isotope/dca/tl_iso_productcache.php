1578914b1fb7b9ff015827b1429cab9977a5b9c7
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-04-01 11:03:05 +0200

    Add uniqid field to product cache

--- system/modules/isotope/dca/tl_iso_productcache.php
+++ system/modules/isotope/dca/tl_iso_productcache.php
@@ -23,8 +23,9 @@ $GLOBALS['TL_DCA']['tl_iso_productcache'] = array
         (
             'keys' => array
             (
-                'id'                                                    => 'primary',
-                'page_id,module_id,requestcache_id,keywords,expires'    => 'index',
+                'id'                                                 => 'primary',
+                'uniqid,requestcache_id,keywords,expires,groups'     => 'index',
+                'page_id,module_id,requestcache_id,keywords,expires' => 'index',
             )
         ),
     ),
@@ -36,17 +37,9 @@ $GLOBALS['TL_DCA']['tl_iso_productcache'] = array
         (
             'sql'                   =>  "int(10) unsigned NOT NULL auto_increment",
         ),
-        'page_id' => array
+        'uniqid' => array
         (
-            'foreignKey'            => 'tl_page.title',
-            'sql'                   =>  "int(10) unsigned NOT NULL default '0'",
-            'relation'              => array('type'=>'hasOne', 'load'=>'lazy'),
-        ),
-        'module_id' => array
-        (
-            'foreignKey'            => 'tl_module.name',
-            'sql'                   =>  "int(10) unsigned NOT NULL default '0'",
-            'relation'              => array('type'=>'hasOne', 'load'=>'lazy'),
+            'sql'                   =>  "varchar(32) NOT NULL default ''",
         ),
         'requestcache_id' => array
         (
@@ -72,6 +65,18 @@ $GLOBALS['TL_DCA']['tl_iso_productcache'] = array
         'expires' => array
         (
             'sql'                   =>  "int(10) unsigned NOT NULL default '0'",
+        ),
+        'page_id' => array
+        (
+            'foreignKey'            => 'tl_page.title',
+            'sql'                   =>  "int(10) unsigned NOT NULL default '0'",
+            'relation'              => array('type'=>'hasOne', 'load'=>'lazy'),
+        ),
+        'module_id' => array
+        (
+            'foreignKey'            => 'tl_module.name',
+            'sql'                   =>  "int(10) unsigned NOT NULL default '0'",
+            'relation'              => array('type'=>'hasOne', 'load'=>'lazy'),
         ),
     ),
 );
