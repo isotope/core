9358534ea600e5a2500eed499a7fddb68c5a1e91
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-06-22 10:36:06 +0200

    Update cumulative filter fields settings for new configuration wizard

--- system/modules/isotope/library/Isotope/Upgrade/To0020030006.php
+++ system/modules/isotope/library/Isotope/Upgrade/To0020030006.php
@@ -15,14 +15,62 @@ namespace Isotope\Upgrade;
 
 class To0020030006 extends Base
 {
+    /**
+     * @var \Contao\Database
+     */
+    private $db;
 
     public function run($blnInstalled)
     {
-        if ($blnInstalled) {
-            // Will update the field definition
-            $this->renameDatabaseField('uniqid', 'uniqid', 'tl_iso_product_collection');
+        $this->db = \Database::getInstance();
 
-            \Database::getInstance()->query("UPDATE tl_iso_product_collection SET uniqid=NULL WHERE uniqid=''");
+        if ($blnInstalled) {
+            $this->updateProductCollectionUuid();
+            $this->updateCumulativeFilterFields();
+        }
+    }
+
+    /**
+     * tl_iso_product_collection.uniqid is NULL by default in Isotope 2.3
+     */
+    private function updateProductCollectionUuid()
+    {
+        // Will update the field definition
+        $this->renameDatabaseField('uniqid', 'uniqid', 'tl_iso_product_collection');
+
+        $this->db->query("UPDATE tl_iso_product_collection SET uniqid=NULL WHERE uniqid=''");
+    }
+
+    /**
+     * Convert iso_filterFields configuration for new cumulative filter
+     */
+    private function updateCumulativeFilterFields()
+    {
+        if ($this->createDatabaseField('iso_cumulativeFields', 'tl_module')) {
+            $modules = $this->db->query(
+                "SELECT id, iso_filterFields FROM tl_module WHERE type='iso_cumulativefilter'"
+            );
+
+            while ($modules->next()) {
+                $fields = deserialize($modules->iso_filterFields);
+
+                if (!empty($fields) && is_array($fields)) {
+                    $config = array();
+
+                    foreach ($fields as $field) {
+                        $config[] = array(
+                            'attribute'  => $field,
+                            'queryType'  => 'and',
+                            'matchCount' => 'none',
+                        );
+                    }
+
+                    $this->db
+                        ->prepare("UPDATE tl_module SET iso_cumulativeFields=? WHERE id=?")
+                        ->execute(serialize($config), $modules->id)
+                    ;
+                }
+            }
         }
     }
 }
