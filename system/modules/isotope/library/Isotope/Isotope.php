bf1c40ca4f7b16a6409adeb78ce69a0386b0143b
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-09-15 11:18:46 +0200

    Starting 2.3.2 hotfix branch

--- system/modules/isotope/library/Isotope/Isotope.php
+++ system/modules/isotope/library/Isotope/Isotope.php
@@ -40,7 +40,7 @@ class Isotope extends \Controller
     /**
      * Isotope version
      */
-    const VERSION = '2.3.1';
+    const VERSION = '2.3.2';
 
     /**
      * True if the system has been initialized
