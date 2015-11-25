23bfc78fb4d317e89236e1ce825ad6cf635098a2
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-11-25 15:29:46 +0100

    Starting version 2.4

--- system/modules/isotope/library/Isotope/Isotope.php
+++ system/modules/isotope/library/Isotope/Isotope.php
@@ -40,7 +40,7 @@ class Isotope extends \Controller
     /**
      * Isotope version
      */
-    const VERSION = '2.3.1';
+    const VERSION = '2.4.0-dev';
 
     /**
      * True if the system has been initialized
