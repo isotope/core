c0cb847b15d93ac1cd1314df76b0c0e17b4793b9
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2016-03-16 11:57:29 +0100

    Made the FileTree attribute sortable (#1561)

--- system/modules/isotope/library/Isotope/Model/Attribute/FileTree.php
+++ system/modules/isotope/library/Isotope/Model/Attribute/FileTree.php
@@ -36,6 +36,10 @@ class FileTree extends Attribute implements IsotopeAttribute
             $arrData['fields'][$this->field_name]['eval']['multiple'] = true;
             $arrData['fields'][$this->field_name]['sql'] = "blob NULL";
         }
+
+        // Make the field sortable
+        $arrData['fields'][$this->field_name]['eval']['orderField'] = $this->getOrderFieldName();
+        $arrData['fields'][$this->getOrderFieldName()]              = ['sql' => "blob NULL"];
     }
 
     public function generate(IsotopeProduct $objProduct, array $arrOptions = array())
@@ -46,12 +50,57 @@ class FileTree extends Attribute implements IsotopeAttribute
             $varValue = deserialize($varValue, true);
         }
 
-        $objFiles = \FilesModel::findMultipleByIds((array) $varValue);
+        $objFiles = \FilesModel::findMultipleByUuids((array) $varValue);
 
         if (null !== $objFiles) {
-            return $this->generateList($objFiles->fetchEach('path'));
+            $files = [];
+
+            /** @var \FilesModel $objFile */
+            foreach ($objFiles as $objFile) {
+                $files[$objFile->uuid] = $objFile->path;
+            }
+
+            // Order the files
+            if (($orderSource = $objProduct->{$this->getOrderFieldName()}) != '') {
+                $tmp = deserialize($orderSource);
+
+                if (!empty($tmp) && is_array($tmp)) {
+                    // Remove all values
+                    $order = array_map(function () {
+                    }, array_flip($tmp));
+
+                    // Move the matching elements to their position in $order
+                    foreach ($files as $k => $v) {
+                        if (array_key_exists($k, $order)) {
+                            $order[$k] = $v;
+                            unset($files[$k]);
+                        }
+                    }
+
+                    // Append the left-over images at the end
+                    if (count($files) > 0) {
+                        $order = array_merge($order, array_values($files));
+                    }
+
+                    // Remove empty (unreplaced) entries
+                    $files = array_values(array_filter($order));
+                    unset($order);
+                }
+            }
+
+            return $this->generateList(array_values($files));
         }
 
         return '';
+    }
+
+    /**
+     * Get the order field name
+     *
+     * @return string
+     */
+    protected function getOrderFieldName()
+    {
+        return $this->field_name.'_order';
     }
 }
