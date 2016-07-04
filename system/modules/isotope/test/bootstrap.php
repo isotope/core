03e95b324af6d0a3429442d90f386cbcf52de2f8
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-07-04 11:37:50 +0200

    Fixed unit tests for Contao 4

--- system/modules/isotope/test/bootstrap.php
+++ system/modules/isotope/test/bootstrap.php
@@ -9,10 +9,15 @@
  * @link       http://isotopeecommerce.org
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
-define('TL_MODE', 'ISO_TEST');
-require __DIR__.'/../../../initialize.php';
 
-NamespaceClassLoader::add('Isotope\Test', 'system/modules/isotope/tests');
+define('TL_MODE', 'ISO_TEST');
+define('TL_SCRIPT', 'system/modules/isotope/test/bootstrap.php');
+
+require_once(__DIR__ . '/../initialize.php');
+
+if (class_exists('NamespaceClassLoader')) {
+    NamespaceClassLoader::add('Isotope\Test', 'system/modules/isotope/test/tests');
+}
 
 $GLOBALS['TL_LANGUAGE'] = 'en';
 $GLOBALS['TL_CONFIG']['displayErrors'] = true;
