0db25c58eb54116eeaccad8f29ad099d3143f8e9
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-10-03 16:00:21 +0200

    Remove Postfinance FIS integration that was never completed

--- system/modules/isotope/library/Isotope/Model/Payment/Postfinance.php
+++ system/modules/isotope/library/Isotope/Model/Payment/Postfinance.php
@@ -11,9 +11,7 @@
 
 namespace Isotope\Model\Payment;
 
-use Haste\Util\StringUtil;
 use Isotope\Interfaces\IsotopePurchasableCollection;
-use Isotope\Isotope;
 
 /**
  * Handle Postfinance (Swiss Post) payments
@@ -138,76 +136,6 @@ class Postfinance extends PSP
             );
         }
 
-        // @todo: Activate this as soon as PostFinance has fixed the issues with FIS
-        // integration on their side
-        //$arrParams = array_merge($arrParams, $this->prepareFISParams($objOrder));
-
         return $arrParams;
-    }
-
-    /**
-     * Prepare FIS params
-     *
-     * @param IsotopePurchasableCollection $objOrder
-     *
-     * @return array
-     */
-    private function prepareFISParams(IsotopePurchasableCollection $objOrder)
-    {
-        $objBillingAddress  = $objOrder->getBillingAddress();
-        $objShippingAddress = $objOrder->getShippingAddress();
-
-        $arrInvoice = array
-        (
-            // Mandatory fields
-            'ECOM_BILLTO_POSTAL_NAME_FIRST'     => substr($objBillingAddress->firstname, 0, 50),
-            'ECOM_BILLTO_POSTAL_NAME_LAST'      => substr($objBillingAddress->lastname, 0, 50),
-            'ECOM_SHIPTO_POSTAL_STREET_LINE1'   => $objShippingAddress->street_1,
-            'ECOM_SHIPTO_POSTAL_POSTALCODE'     => $objShippingAddress->postal,
-            'ECOM_SHIPTO_POSTAL_CITY'           => $objShippingAddress->city,
-            'ECOM_SHIPTO_POSTAL_COUNTRYCODE'    => strtoupper($objShippingAddress->country),
-            'ECOM_SHIPTO_DOB'                   => date('d/m/Y', $objShippingAddress->dateOfBirth),
-            // This key is mandatory and just has to be unique (17 chars)
-            'REF_CUSTOMERID'                    => substr('psp_' . $this->id . '_' . $objOrder->getId() . '_' . $objOrder->getUniqueId(), 0, 17),
-
-            // Additional fields, not mandatory
-            'ECOM_CONSUMER_GENDER'              => 'male' === $objBillingAddress->gender ? 'M' : 'F',
-
-            // We do not add "ECOM_SHIPTO_COMPANY" here because B2B sometimes may require up to 24 hours
-            // to check solvency which is not acceptable for an online shop
-        );
-
-        $arrOrder = array();
-        $i = 1;
-
-        // Need to take the items from the cart as they're not transferred to the order here yet
-        // @todo this is no longer true, and the price should probably be taken from the collection item ($objItem->getPrice())
-        foreach (Isotope::getCart()->getItems() as $objItem) {
-
-            $objPrice = $objItem->getProduct()->getPrice();
-            $fltVat = Isotope::roundPrice((100 / $objPrice->getNetAmount() * $objPrice->getGrossAmount()) - 100, false);
-
-            $arrOrder['ITEMID' . $i]   = $objItem->id;
-            $arrOrder['ITEMNAME' . $i] = substr(
-                StringUtil::convertToText(
-                    $objItem->getName(),
-                    StringUtil::NO_TAGS | StringUtil::NO_BREAKS | StringUtil::NO_INSERTTAGS | StringUtil::NO_ENTITIES
-                ),
-                40
-            );
-            $arrOrder['ITEMPRICE' . $i]   = $objPrice->getNetAmount();
-            $arrOrder['ITEMQUANT' . $i]   = $objItem->quantity;
-            $arrOrder['ITEMVATCODE' . $i] = $fltVat . '%';
-            $arrOrder['ITEMVAT' . $i]     = Isotope::roundPrice(
-                $objPrice->getGrossAmount() - $objPrice->getNetAmount(),
-                false
-            );
-            $arrOrder['FACEXCL' . $i]     = $objPrice->getNetAmount();
-            $arrOrder['FACTOTAL' . $i]    = $objPrice->getGrossAmount();
-
-            ++$i;
-        }
-
-        return array_merge($arrInvoice, $arrOrder);
     }
 }
