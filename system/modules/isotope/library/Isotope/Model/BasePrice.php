c77fc8d738d568a6c5d97ce5370c87add6cd4087
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-03-18 09:24:06 +0100

    Updated copyright

--- system/modules/isotope/library/Isotope/Model/BasePrice.php
+++ system/modules/isotope/library/Isotope/Model/BasePrice.php
@@ -3,7 +3,8 @@
 /*
  * Isotope eCommerce for Contao Open Source CMS
  *
- * @copyright  Copyright (C) 2009 - 2019 terminal42 gmbh & Isotope eCommerce Workgroup
+ * Copyright (C) 2009 - 2019 terminal42 gmbh & Isotope eCommerce Workgroup
+ *
  * @link       https://isotopeecommerce.org
  * @license    https://opensource.org/licenses/lgpl-3.0.html
  */
