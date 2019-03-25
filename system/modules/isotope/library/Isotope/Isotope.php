0eb6bba19ce4d9d8a5c5ecd06c06e3f90d98eaba
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-03-25 10:20:16 +0100

    Release 2.5.12

--- system/modules/isotope/library/Isotope/Isotope.php
+++ system/modules/isotope/library/Isotope/Isotope.php
@@ -44,7 +44,7 @@ class Isotope extends \Controller
     /**
      * Isotope version
      */
-    const VERSION = '2.5.11';
+    const VERSION = '2.5.12';
 
     /**
      * True if the system has been initialized
