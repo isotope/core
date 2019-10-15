d78de9a7868f0e4be80656130d2b87b5838b4f25
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-10-15 10:36:49 +0200

    Release 2.6.3

--- system/modules/isotope/library/Isotope/Isotope.php
+++ system/modules/isotope/library/Isotope/Isotope.php
@@ -44,7 +44,7 @@ class Isotope extends \Controller
     /**
      * Isotope version
      */
-    const VERSION = '2.6.2';
+    const VERSION = '2.6.3';
 
     /**
      * True if the system has been initialized
