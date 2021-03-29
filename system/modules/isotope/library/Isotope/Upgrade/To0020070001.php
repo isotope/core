6b1c9f029c932d082f3e78eed9fdac3a5e60b318
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-03-29 09:45:01 +0200

    CS

--- system/modules/isotope/library/Isotope/Upgrade/To0020070001.php
+++ system/modules/isotope/library/Isotope/Upgrade/To0020070001.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * Isotope eCommerce for Contao Open Source CMS
- *
- * Copyright (C) 2009-2017 terminal42 gmbh & Isotope eCommerce Workgroup
- *
- * @link       https://isotopeecommerce.org
- * @license    https://opensource.org/licenses/lgpl-3.0.html
- */
-
-/**
- * Isotope eCommerce for Contao Open Source CMS
- *
- * Copyright (C) 2009-2016 terminal42 gmbh & Isotope eCommerce Workgroup
- *
- * @link       https://isotopeecommerce.org
- * @license    https://opensource.org/licenses/lgpl-3.0.html
- */
 
 namespace Isotope\Upgrade;
 
