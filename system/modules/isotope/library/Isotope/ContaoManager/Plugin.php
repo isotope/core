746e4af6a6e5a9516f19490916f6fa239a822c07
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-06-10 14:07:00 +0200

    Use a YAML file to load services

--- system/modules/isotope/library/Isotope/ContaoManager/Plugin.php
+++ system/modules/isotope/library/Isotope/ContaoManager/Plugin.php
@@ -13,8 +13,6 @@ namespace Isotope\ContaoManager;
 
 use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 
 class Plugin implements ConfigPluginInterface
 {
@@ -23,19 +21,6 @@ class Plugin implements ConfigPluginInterface
      */
     public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig)
     {
-        $loader->load(function (ContainerBuilder $container) {
-            $container->setDefinition(
-                'isotope.listener.console',
-                (new Definition('Isotope\EventListener\SymlinkCommandListener'))
-                    ->setArguments(['%kernel.project_dir%'])
-                    ->addTag('kernel.event_listener', ['event' => 'console.terminate'])
-            );
-
-            $container->setDefinition(
-                'isotope.listener.frontend_preview',
-                (new Definition('Isotope\EventListener\FrontendPreviewListener'))
-                    ->addTag('kernel.event_listener', ['event' => 'contao.preview_url_create', 'method' => 'onPreviewUrlCreate'])
-            );
-        });
+        $loader->load(__DIR__.'/../../../config/services.yaml');
     }
 }
