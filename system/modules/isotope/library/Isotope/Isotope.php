440edf7555999e95d13e414accc5f67c5b1bc0b8
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-07-06 17:27:51 +0200

    Release 2.7.3

--- system/modules/isotope/library/Isotope/Isotope.php
+++ system/modules/isotope/library/Isotope/Isotope.php
@@ -44,7 +44,7 @@ class Isotope extends \Controller
     /**
      * Isotope version
      */
-    const VERSION = '2.7.2';
+    const VERSION = '2.7.3';
 
     /**
      * True if the system has been initialized
