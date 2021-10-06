41b2208fd19e444cdca116e131a827793db451c9
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-10-06 13:30:36 +0200

    Added payment method and card number to Datatrans payment

--- system/modules/isotope/library/Isotope/Model/Payment/Datatrans.php
+++ system/modules/isotope/library/Isotope/Model/Payment/Datatrans.php
@@ -11,11 +11,18 @@
 
 namespace Isotope\Model\Payment;
 
+use Contao\Environment;
+use Contao\Input;
+use Contao\Module;
 use Contao\StringUtil;
+use Contao\System;
+use Isotope\Interfaces\IsotopeBackendInterface;
+use Isotope\Interfaces\IsotopeNotificationTokens;
 use Isotope\Interfaces\IsotopeProductCollection;
 use Isotope\Interfaces\IsotopePurchasableCollection;
 use Isotope\Model\ProductCollection\Order;
 use Isotope\Module\Checkout;
+use Isotope\Template;
 
 /**
  * Datatrans payment method
@@ -25,8 +32,54 @@ use Isotope\Module\Checkout;
  * @property string $datatrans_hash_method
  * @property string $datatrans_hash_convert
  */
-class Datatrans extends Postsale
+class Datatrans extends Postsale implements IsotopeNotificationTokens, IsotopeBackendInterface
 {
+    // see https://docs.datatrans.ch/docs/payment-methods
+    public const PAYMENT_METHODS = [
+        'ECA' => 'MasterCard',
+        'VIS' => 'VISA',
+        'AMX' => 'American Express',
+        'CUP' => 'UnionPay',
+        'DIN' => 'Diners',
+        'DIS' => 'Discover',
+        'JCB' => 'JCB',
+        'MAU' => 'Maestro',
+        'DNK' => 'Dankort',
+        'UAP' => 'Airplus',
+        'BON' => 'Boncard / Lunch-Check',
+        'MYO' => 'Manor MyOne',
+        'MMS' => 'Mediamarkt Shopping Card',
+        'AZP' => 'Amazon Pay',
+        'APL' => 'Apple Pay',
+        'ACC' => 'Availabill',
+        'INT' => 'Byjuno',
+        'DVI' => 'CRIF',
+        'CFY' => 'Cryptocurrencies',
+        'EPS' => 'EPS',
+        'GEP' => 'EPS',
+        'PAY' => 'Google Pay',
+        'GPA' => 'Giropay',
+        'DEA' => 'iDEAL',
+        'KLN' => 'Klarna',
+        'MDP' => 'Migros Bank E-Pay',
+        'PAP' => 'PayPal',
+        'PSC' => 'paysafecard',
+        'PFC' => 'PostFinance Card',
+        'PEF' => 'PostFinance E-Finance',
+        'MFX' => 'Powerpay',
+        'MFG' => 'Powerpay Authorization',
+        'MFA' => 'Powerpay Credit Check',
+        'MPX' => 'Paycard',
+        'REK' => 'Reka',
+        'SAM' => 'Samsung Pay',
+        'ELV' => 'SEPA',
+        'DIB' => 'Sofort',
+        'SWB' => 'swissbilling',
+        'ESY' => 'Swisscom Pay',
+        'SWP' => 'SwissPass',
+        'TWI' => 'Twint',
+    ];
+
     /**
      * Perform server to server data check
      *
@@ -35,25 +88,25 @@ class Datatrans extends Postsale
     public function processPostsale(IsotopeProductCollection $objOrder)
     {
         if (!$objOrder instanceof IsotopePurchasableCollection) {
-            \System::log('Product collection ID "' . $objOrder->getId() . '" is not purchasable', __METHOD__, TL_ERROR);
+            System::log('Product collection ID "' . $objOrder->getId() . '" is not purchasable', __METHOD__, TL_ERROR);
             return;
         }
 
         // Verify payment status
-        if (\Input::post('status') !== 'success') {
-            \System::log('Payment for order ID "' . \Input::post('refno') . '" failed.', __METHOD__, TL_ERROR);
-            $this->debugLog('Expected status "success", got "'.\Input::post('status').'"');
+        if (Input::post('status') !== 'success') {
+            System::log('Payment for order ID "' . Input::post('refno') . '" failed.', __METHOD__, TL_ERROR);
+            $this->debugLog('Expected status "success", got "'.Input::post('status').'"');
             $this->debugLog($_POST);
 
             return;
         }
 
         // Validate HMAC sign
-        $hash = $this->createHash($this->datatrans_id . \Input::post('amount') . \Input::post('currency') . \Input::post('uppTransactionId'));
+        $hash = $this->createHash($this->datatrans_id . Input::post('amount') . Input::post('currency') . Input::post('uppTransactionId'));
 
-        if (\Input::post('sign2') != $hash) {
-            \System::log('Invalid HMAC signature for Order ID ' . \Input::post('refno'), __METHOD__, TL_ERROR);
-            $this->debugLog('Expected hash "'.$hash.'", got "'.\Input::post('sign2').'"');
+        if (Input::post('sign2') != $hash) {
+            System::log('Invalid HMAC signature for Order ID ' . Input::post('refno'), __METHOD__, TL_ERROR);
+            $this->debugLog('Expected hash "'.$hash.'", got "'.Input::post('sign2').'"');
             $this->debugLog($_POST);
 
             return;
@@ -72,18 +125,24 @@ class Datatrans extends Postsale
         }
 
         if ($objOrder->isCheckoutComplete()) {
-            \System::log('Postsale checkout for Order ID "' . $objOrder->getId() . '" already completed', __METHOD__, TL_ERROR);
+            System::log('Postsale checkout for Order ID "' . $objOrder->getId() . '" already completed', __METHOD__, TL_ERROR);
             return;
         }
 
         if (!$objOrder->checkout()) {
-            \System::log('Postsale checkout for Order ID "' . \Input::post('refno') . '" failed', __METHOD__, TL_ERROR);
+            System::log('Postsale checkout for Order ID "' . Input::post('refno') . '" failed', __METHOD__, TL_ERROR);
 
             return;
         }
 
-        $objOrder->setDatePaid(time());
-        $objOrder->updateOrderStatus($this->new_order_status);
+        $objOrder->updateOrderStatus([
+            'order_status' => $this->new_order_status,
+            'date_paid' => time(),
+            'payment_data' => [
+                'pmethod' => Input::post('pmethod'),
+                'cardno' => Input::post('cardno'),
+            ]
+        ]);
 
         $objOrder->save();
     }
@@ -93,16 +152,16 @@ class Datatrans extends Postsale
      */
     public function getPostsaleOrder()
     {
-        return Order::findByPk(\Input::post('refno'));
+        return Order::findByPk(Input::post('refno'));
     }
 
     /**
      * @inheritdoc
      */
-    public function checkoutForm(IsotopeProductCollection $objOrder, \Module $objModule)
+    public function checkoutForm(IsotopeProductCollection $objOrder, Module $objModule)
     {
         if (!$objOrder instanceof IsotopePurchasableCollection) {
-            \System::log('Product collection ID "' . $objOrder->getId() . '" is not purchasable', __METHOD__, TL_ERROR);
+            System::log('Product collection ID "' . $objOrder->getId() . '" is not purchasable', __METHOD__, TL_ERROR);
             return false;
         }
 
@@ -127,9 +186,9 @@ class Datatrans extends Postsale
             'uppCustomerZipCode'    => $objAddress->postal,
             'uppCustomerPhone'      => $objAddress->phone,
             'uppCustomerEmail'      => $objAddress->email,
-            'successUrl'            => ampersand(\Environment::get('base') . Checkout::generateUrlForStep('complete', $objOrder)),
-            'errorUrl'              => ampersand(\Environment::get('base') . Checkout::generateUrlForStep('failed')),
-            'cancelUrl'             => ampersand(\Environment::get('base') . Checkout::generateUrlForStep('failed')),
+            'successUrl'            => ampersand(Environment::get('base') . Checkout::generateUrlForStep('complete', $objOrder)),
+            'errorUrl'              => ampersand(Environment::get('base') . Checkout::generateUrlForStep('failed')),
+            'cancelUrl'             => ampersand(Environment::get('base') . Checkout::generateUrlForStep('failed')),
             'mod'                   => 'pay',
             'id'                    => $this->id,
         );
@@ -137,7 +196,7 @@ class Datatrans extends Postsale
         // Security signature (see Security Level 2)
         $arrParams['sign'] = $this->createHash($arrParams['merchantId'] . $arrParams['amount'] . $arrParams['currency'] . $arrParams['refno']);
 
-        $objTemplate           = new \Isotope\Template('iso_payment_datatrans');
+        $objTemplate           = new Template('iso_payment_datatrans');
         $objTemplate->id       = $this->id;
         $objTemplate->action   = ('https://' . ($this->debug ? 'pay.sandbox' : 'pay') . '.datatrans.com/upp/jsp/upStart.jsp');
         $objTemplate->params   = $arrParams;
@@ -158,9 +217,9 @@ class Datatrans extends Postsale
     private function validateParameters(array $arrData)
     {
         foreach ($arrData as $key => $value) {
-            if (\Input::post($key) != $value) {
-                \System::log(
-                    'Wrong data for parameter "' . $key . '" (Order ID "' . \Input::post('refno') . ').',
+            if (Input::post($key) != $value) {
+                System::log(
+                    'Wrong data for parameter "' . $key . '" (Order ID "' . Input::post('refno') . ').',
                     __METHOD__,
                     TL_ERROR
                 );
@@ -192,5 +251,59 @@ class Datatrans extends Postsale
             $value,
             $this->datatrans_hash_convert ? hex2bin($this->datatrans_sign) : $this->datatrans_sign
         );
+    }
+
+    public function getNotificationTokens(IsotopeProductCollection $collection): array
+    {
+        $tokens = [];
+        $paymentData = StringUtil::deserialize($collection->payment_data);
+
+        if (!empty($paymentData) && \is_array($paymentData)) {
+            foreach ($paymentData as $k => $v) {
+                $tokens['payment_datatrans_'.$k] = $v;
+            }
+        }
+
+        return $tokens;
+    }
+
+    public function hasBackendInterface(int $collectionId): bool
+    {
+        return true;
+    }
+
+    public function renderBackendInterface(int $orderId): string
+    {
+        if (($objOrder = Order::findByPk($orderId)) === null) {
+            return '<p class="tl_gerror">' . $GLOBALS['TL_LANG']['MSC']['backendPaymentNotFound'] . '</p>';
+        }
+
+        $arrPayment = StringUtil::deserialize($objOrder->payment_data);
+
+        if (empty($arrPayment) || !\is_array($arrPayment)) {
+            return '<p class="tl_gerror">' . $GLOBALS['TL_LANG']['MSC']['backendPaymentNotFound'] . '</p>';
+        }
+
+        $strBuffer = '
+<div id="tl_buttons">
+<a href="' . ampersand(str_replace('&key=payment', '', Environment::get('request'))) . '" class="header_back" title="' . StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['backBT']) . '">' . $GLOBALS['TL_LANG']['MSC']['backBT'] . '</a>
+</div>
+
+<h2 class="sub_headline">' . $this->name . ' (' . $GLOBALS['TL_LANG']['MODEL']['tl_iso_payment']['datatrans'][0] . ')' . '</h2>
+
+<table class="tl_show">
+  <tbody>
+  <tr>
+    <td class="tl_bg"><span class="tl_label">Zahlungsmethode: </span></td>
+    <td class="tl_bg">' . (self::PAYMENT_METHODS[$arrPayment['pmethod']] ?? $arrPayment['pmethod']) . '</td>
+  </tr>
+  <tr>
+    <td><span class="tl_label">Kartennummer: </span></td>
+    <td>' . ($arrPayment['cardno'] ?: '-') . '</td>
+  </tr>
+</tbody></table>
+</div>';
+
+        return $strBuffer;
     }
 }
