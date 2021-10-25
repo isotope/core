acba53aff9696d1c0e981db1407e7935b3441a1a
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-10-25 09:28:44 +0200

    Fixed field length for price information

--- system/modules/isotope/dca/tl_member_group.php
+++ system/modules/isotope/dca/tl_member_group.php
@@ -16,5 +16,5 @@ $GLOBALS['TL_DCA']['tl_member_group']['fields']['iso_priceDisplay'] = array
     'options'               => array(\Isotope\Model\Config::PRICE_DISPLAY_NET, \Isotope\Model\Config::PRICE_DISPLAY_GROSS, \Isotope\Model\Config::PRICE_DISPLAY_FIXED),
     'reference'             => &$GLOBALS['TL_LANG']['MSC']['iso_priceDisplay'],
     'eval'                  => array('includeBlankOption'=>true, 'tl_class'=>'w50', 'helpwizard'=>true),
-    'sql'                   => "varchar(9) NOT NULL default ''",
+    'sql'                   => "varchar(8) NOT NULL default ''",
 );
