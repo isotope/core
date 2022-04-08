e7f32c0dba4788db2e633cb91eea7ee8e5aa7447
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-04-08 14:41:24 +0200

    Fix global array access for delete button
    see isotope/core#2276

--- system/modules/isotope/dca/tl_iso_address.php
+++ system/modules/isotope/dca/tl_iso_address.php
@@ -74,7 +74,7 @@ $GLOBALS['TL_DCA']['tl_iso_address'] = array
                 'label'             => &$GLOBALS['TL_LANG']['tl_iso_address']['delete'],
                 'href'              => 'act=delete',
                 'icon'              => 'delete.gif',
-                'attributes'        => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
+                'attributes'        => 'onclick="if (!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '') . '\')) return false; Backend.getScrollOffset();"'
             ),
             'show' => array
             (
