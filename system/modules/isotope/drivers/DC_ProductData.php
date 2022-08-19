91d331ac3eee46a94ab23485eb83481a29ed9bfd
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2022-08-19 12:05:31 +0100

    fix warning

--- system/modules/isotope/drivers/DC_ProductData.php
+++ system/modules/isotope/drivers/DC_ProductData.php
@@ -1594,7 +1594,7 @@ class DC_ProductData extends \DC_Table
                             }
 
                             $implode = static function ($v) use (&$implode) {
-                                return implode(', ', array_map(static function($vv) {
+                                return implode(', ', array_map(static function($vv) use ($implode) {
                                     return \is_array($vv) ? $implode($vv) : $vv;
                                 }, $v));
                             };
