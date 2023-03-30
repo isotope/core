ad5c03adc5014ec43f1e8047eb0a6a846b3b082a
fritzmg <fmg@inspiredminds.at>
2023-03-30 21:04:20 +0100

    fix various PHP 8 issues under C4.13

--- system/modules/isotope/library/Isotope/Model/Document/Standard.php
+++ system/modules/isotope/library/Isotope/Model/Document/Standard.php
@@ -177,9 +177,9 @@ class Standard extends Document implements IsotopeDocument
         $objTemplate->collection    = $objCollection;
         $objTemplate->config        = $objCollection->getConfig();
         $objTemplate->page          = $objPage;
-        $objTemplate->dateFormat    = $objPage->dateFormat ?: $GLOBALS['TL_CONFIG']['dateFormat'];
-        $objTemplate->timeFormat    = $objPage->timeFormat ?: $GLOBALS['TL_CONFIG']['timeFormat'];
-        $objTemplate->datimFormat   = $objPage->datimFormat ?: $GLOBALS['TL_CONFIG']['datimFormat'];
+        $objTemplate->dateFormat    = !empty($objPage->dateFormat) ? $objPage->dateFormat : $GLOBALS['TL_CONFIG']['dateFormat'];
+        $objTemplate->timeFormat    = !empty($objPage->timeFormat) ? $objPage->timeFormat : $GLOBALS['TL_CONFIG']['timeFormat'];
+        $objTemplate->datimFormat   = !empty($objPage->datimFormat) ? $objPage->datimFormat : $GLOBALS['TL_CONFIG']['datimFormat'];
 
         // Render the collection
         $objCollectionTemplate = new Template($this->collectionTpl);
