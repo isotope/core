ad112adec8fe4f195804b281b7225c7ac626c59f
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-06-06 11:52:49 +0200

    Release 2.8.13

--- system/modules/isotope/library/Isotope/Isotope.php
+++ system/modules/isotope/library/Isotope/Isotope.php
@@ -42,7 +42,7 @@ class Isotope extends Controller
     /**
      * Isotope version
      */
-    const VERSION = '2.8.12';
+    const VERSION = '2.8.13';
 
     /**
      * True if the system has been initialized
