3931fe42440c666b266c5d598608665824aed0f0
Tsarma <tsarma@users.noreply.github.com>
2023-06-07 09:22:33 +0200

    Update Standard.php
    Added null coalescing check for alt and desc also. Related to https://github.com/isotope/core/issues/2453

--- system/modules/isotope/library/Isotope/Model/Gallery/Standard.php
+++ system/modules/isotope/library/Isotope/Model/Gallery/Standard.php
@@ -275,8 +275,8 @@ class Standard extends Gallery implements IsotopeGallery
         $objTemplate->file       = $arrFile;
         $objTemplate->src        = $arrFile[$strType] ?? '';
         $objTemplate->size       = $arrFile[$strType . '_size'] ?? '';
-        $objTemplate->alt        = $arrFile['alt'];
-        $objTemplate->title      = $arrFile['desc'];
+        $objTemplate->alt        = $arrFile['alt'] ?? '';
+        $objTemplate->title      = $arrFile['desc'] ?? '';
         $objTemplate->class      = trim(($this->arrData['class'] ?? '') . ' ' . ($arrFile['class'] ?? ''));
 
         // Add the missing data to the picture
