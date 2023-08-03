131442cac3a53ebba5405f35677edaa4d437ea3f
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-08-03 15:33:42 +0200

    Fixed delete button message for attribute options

--- system/modules/isotope/dca/tl_iso_attribute_option.php
+++ system/modules/isotope/dca/tl_iso_attribute_option.php
@@ -83,7 +83,7 @@ $GLOBALS['TL_DCA']['tl_iso_attribute_option'] = array
             (
                 'href'              => 'act=delete',
                 'icon'              => 'delete.svg',
-                'attributes'        => 'onclick="if (!confirm(\'' . ($GLOBALS['TL_LANG']['tl_iso_attribute_option']['deleteConfirm'] ?? '') . '\')) return false; Backend.getScrollOffset();"'
+                'attributes'        => 'onclick="if (!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '') . '\')) return false; Backend.getScrollOffset();"'
             ),
             'toggle' => array
             (
