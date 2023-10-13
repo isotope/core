09414d13eca5a5e9bc2bfa80e81a29aaaae0b736
Ernst Jendritzki <ernstjendritzki@web.de>
2023-10-13 23:56:44 +0200

    exchange TL_MODEby ScopeMatcher service

--- system/modules/isotope/config/config.php
+++ system/modules/isotope/config/config.php
@@ -45,7 +45,8 @@ array_insert($GLOBALS['BE_MOD']['isotope'], 0, array
 
 $GLOBALS['BE_MOD']['accounts']['member']['tables'][] = \Isotope\Model\Address::getTable();
 
-if ('BE' === TL_MODE) {
+if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
+    
     $GLOBALS['TL_CSS'][] = 'system/modules/isotope/assets/css/backend.css|static';
 }
 
@@ -483,12 +484,15 @@ if (\Contao\Config::getInstance()->isComplete()) {
     $GLOBALS['TL_HOOKS']['changelanguageNavigation'][]      = array('Isotope\EventListener\ChangeLanguageListener', '__invoke');
 
     // Set module and module id for payment and/or shipping modules
-    if ('FE' === TL_MODE) {
+    if (System::getContainer()->get('contao.routing.scope_matcher')->isFrontendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
+
         // Only limit countries in FE
         $GLOBALS['TL_HOOKS']['loadDataContainer'][]         = array('Isotope\Backend\Member\Callback', 'limitCountries');
     }
 
-    if ('BE' === TL_MODE) {
+
+    if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
+
         // Type agent help is only needed in back end
         $GLOBALS['TL_HOOKS']['loadDataContainer'][]         = array('Isotope\Backend', 'loadTypeAgentHelp');
         $GLOBALS['TL_HOOKS']['loadLanguageFile'][]          = array('Isotope\Backend\ProductType\Help', 'initializeWizard');
