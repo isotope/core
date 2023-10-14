2afbc84b92f1eb220025937797e17186f368e335
Ernst Jendritzki <ernstjendritzki@web.de>
2023-10-15 00:53:16 +0200

    Replace array_insert by ArrayUtil::arrayInsert

--- system/modules/isotope/config/config.php
+++ system/modules/isotope/config/config.php
@@ -9,15 +9,17 @@
  * @license    https://opensource.org/licenses/lgpl-3.0.html
  */
 
+use Contao\ArrayUtil;
+
 /**
  * Backend modules
  */
 if (!\is_array($GLOBALS['BE_MOD']['isotope'] ?? null))
 {
-    array_insert($GLOBALS['BE_MOD'], 1, array('isotope' => array()));
+    ArrayUtil::arrayInsert($GLOBALS['BE_MOD'], 1, array('isotope' => array()));
 }
 
-array_insert($GLOBALS['BE_MOD']['isotope'], 0, array
+ArrayUtil::arrayInsert($GLOBALS['BE_MOD']['isotope'], 0, array
 (
     'iso_products' => array
     (
