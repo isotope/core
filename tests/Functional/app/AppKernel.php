1df7ab5d7a787651a79750a75efd84da9064bca8
Ernst Jendritzki <ernstjendritzki@web.de>
2023-10-20 22:50:21 +0200

    add testsuite reset

--- tests/Functional/app/AppKernel.php
+++ tests/Functional/app/AppKernel.php
@@ -5,16 +5,10 @@ declare(strict_types=1);
 /*
  * Isotope eCommerce for Contao Open Source CMS
  *
- *
  * Copyright (C) 2009 - 2019 terminal42 gmbh & Isotope eCommerce Workgroup
  *
- *
  * @link       https://isotopeecommerce.org
- *
- *
  * @license    https://opensource.org/licenses/lgpl-3.0.html
- *
- *
  */
 
 namespace Isotope\Tests\Functional\app;
