c29744d5e647d9031efb86e16679192b809eec3b
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-12-15 16:29:38 +0100

    Allow to filter orders by shop config

--- system/modules/isotope/dca/tl_iso_product_collection.php
+++ system/modules/isotope/dca/tl_iso_product_collection.php
@@ -263,6 +263,7 @@ $GLOBALS['TL_DCA']['tl_iso_product_collection'] = array
         ),
         'config_id' => array
         (
+            'filter'                => true,
             'foreignKey'            => \Isotope\Model\Config::getTable().'.name',
             'sql'                   => "int(10) unsigned NOT NULL default '0'",
             'relation'              => array('type'=>'hasOne', 'load'=>'lazy'),
