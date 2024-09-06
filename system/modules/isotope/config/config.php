0f2d84e0289f5fe2a3a58ac276c35dd42d8f1f44
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-09-06 14:54:34 +0200

    ScopeMatcher does not seem to working during initializeSystem

--- system/modules/isotope/config/config.php
+++ system/modules/isotope/config/config.php
@@ -10,7 +10,6 @@
  */
 
 use Contao\ArrayUtil;
-use Isotope\CompatibilityHelper;
 
 /**
  * Backend modules
@@ -48,7 +47,7 @@ ArrayUtil::arrayInsert($GLOBALS['BE_MOD']['isotope'], 0, array
 
 $GLOBALS['BE_MOD']['accounts']['member']['tables'][] = \Isotope\Model\Address::getTable();
 
-if (CompatibilityHelper::isBackend()) {
+if ('BE' === TL_MODE) {
     $GLOBALS['TL_CSS'][] = 'system/modules/isotope/assets/css/backend.css|static';
 }
 
@@ -488,12 +487,12 @@ if (\Contao\Config::getInstance()->isComplete()) {
     $GLOBALS['TL_HOOKS']['changelanguageNavigation'][]      = array('Isotope\EventListener\ChangeLanguageListener', '__invoke');
 
     // Set module and module id for payment and/or shipping modules
-    if (CompatibilityHelper::isFrontend()) {
+    if ('FE' === TL_MODE) {
         // Only limit countries in FE
         $GLOBALS['TL_HOOKS']['loadDataContainer'][]         = array('Isotope\Backend\Member\Callback', 'limitCountries');
     }
 
-    if (CompatibilityHelper::isBackend()) {
+    if ('BE' === TL_MODE) {
         // Type agent help is only needed in back end
         $GLOBALS['TL_HOOKS']['loadDataContainer'][]         = array('Isotope\Backend', 'loadTypeAgentHelp');
         $GLOBALS['TL_HOOKS']['loadLanguageFile'][]          = array('Isotope\Backend\ProductType\Help', 'initializeWizard');
