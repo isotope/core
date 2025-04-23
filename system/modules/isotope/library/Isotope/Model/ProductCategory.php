20ea8497e0598e5daf76a57fdb08776905e0081f
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-04-23 08:44:36 +0200

    Update comment

--- system/modules/isotope/library/Isotope/Model/ProductCategory.php
+++ system/modules/isotope/library/Isotope/Model/ProductCategory.php
@@ -63,8 +63,9 @@ class ProductCategory extends Model
      */
     public static function findByPidForPublishedPages($intProduct, array $arrOptions = array())
     {
-        // Register the model to prevent a runtime error
+        // Register model to prevent eager loading of product data (see isotope/core#2570)
         Product::findByPk($intProduct);
+
         $arrOptions = static::getFindByPidForPublishedPagesOptions($intProduct, $arrOptions);
 
         return parent::find($arrOptions);
